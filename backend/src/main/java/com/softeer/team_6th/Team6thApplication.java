fedccab0fae0886c8c98425a9826ebb0ea9a0cd6
naver0504 <qortmdwls1234@naver.com>
2025-07-28 13:36:43 +0900

    backend init

---
+++ backend/src/main/java/com/softeer/team_6th/Team6thApplication.java
@@ -0,0 +1,13 @@
+package com.softeer.team_6th;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team6thApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team6thApplication.class, args);
+	}
+
+}
