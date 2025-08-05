f06c8756400c6fe1da97cf00b31ca0331e7c14f8
naver0504 <qortmdwls1234@naver.com>
2025-08-05 14:11:48 +0900

    Revert "feat: Grid JpaRepository 작성"
    This reverts commit 507366f873ebf1ab7e9dcb6d33c8c8d7810a4705.

--- backend/domain-modules/grid-domain/src/main/java/com/softeer/repository/GridJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.softeer.repository;
-
-import com.softeer.entity.GridEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GridJpaRepository extends JpaRepository<GridEntity, Integer> {
-}
