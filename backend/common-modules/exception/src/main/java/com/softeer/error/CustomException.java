bc810ffd4b540d914e97c1086994ba861a318e8d
naver0504 <qortndwls1234@gmail.com>
2025-08-05 23:42:14 +0900

    refactor: RuntimeException -> Throwable에 이미 필드로 message 저장하고 Getter 구현되어있으므로 필드 체거

--- backend/common-modules/exception/src/main/java/com/softeer/error/CustomException.java
+++ backend/common-modules/exception/src/main/java/com/softeer/error/CustomException.java
@@ -4,22 +4,19 @@ public class CustomException extends RuntimeException {
 
     private final int status;
     private final String errorCode;
-    private final String message;
     private final String errorLog;
 
-    public CustomException(int status, String errorCode, String message) {
+    public CustomException(String message, int status, String errorCode) {
         super(message);
         this.status = status;
         this.errorCode = errorCode;
-        this.message = message;
         this.errorLog = null;
     }
 
-    public CustomException(int status, String errorCode, String message, String errorLog) {
+    public CustomException(String message, int status, String errorCode, String errorLog) {
         super(message);
         this.status = status;
         this.errorCode = errorCode;
-        this.message = message;
         this.errorLog = errorLog;
     }
 
