a74ba34ab2c62dbedf3953a723ec144314b7d5d9
bobob0311 <qweasdzxc0311@naver.com>
2025-08-06 18:31:13 +0900

    ✨ / #55 - 프로젝트내 li 태그 공통 atoms 구현

---
+++ frontend/src/components/atoms/Li/TextLi.tsx
@@ -0,0 +1,25 @@
+import { css } from '@emotion/react';
+import { theme } from '../../../theme/theme';
+
+interface PropsState {
+    children?: string;
+}
+export default function TextLi({ children }: PropsState) {
+    return <li css={textStyles}>{children}</li>;
+}
+
+const { colors, typography } = theme;
+
+const textStyles = css`
+    font-family: Pretendard;
+    color: ${colors.greyOpacityWhite[50]};
+    font-size: ${typography.fontSize.label};
+    font-weight: ${typography.fontWeight.bold};
+    font-style: normal;
+    line-height: normal;
+
+    &:hover {
+        color: ${colors.grey[100]};
+        cursor: pointer;
+    }
+`;
