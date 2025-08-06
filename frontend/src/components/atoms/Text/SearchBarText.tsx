feea248db47af1c59dc00d10a5a6a3ad1a5f2f1a
bobob0311 <qweasdzxc0311@naver.com>
2025-08-06 18:33:10 +0900

    ✨ / #55 - SearchBar에 들어가는 text atoms 구현

---
+++ frontend/src/components/atoms/Text/SearchBarText.tsx
@@ -0,0 +1,21 @@
+import { css } from '@emotion/react';
+import { theme } from '../../../theme/theme';
+
+interface PropsState {
+    children?: string;
+}
+
+export default function SearchBarText({ children }: PropsState) {
+    return <span css={textStyles}>{children}</span>;
+}
+
+const { colors, typography } = theme;
+
+const textStyles = css`
+    font-family: Pretendard;
+    color: ${colors.grey[100]};
+    font-size: ${typography.fontSize.label};
+    font-weight: ${typography.fontWeight.bold};
+    font-style: normal;
+    line-height: normal;
+`;
