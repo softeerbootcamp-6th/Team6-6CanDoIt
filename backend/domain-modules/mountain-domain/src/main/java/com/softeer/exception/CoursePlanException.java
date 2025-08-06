5a99ab34a581fb04cdc799073b1897d612061754
Haemulramen <apple11078@gmail.com>
2025-08-06 17:59:00 +0900

    feat: CoursePlanException Enum 추가 (CoursePlan 내부 에러 발생 경우)

---
+++ backend/domain-modules/mountain-domain/src/main/java/com/softeer/exception/CoursePlanException.java
@@ -0,0 +1,33 @@
+package com.softeer.exception;
+
+import com.softeer.error.ExceptionInterface;
+import com.softeer.error.HttpStatus;
+
+public enum CoursePlanException implements ExceptionInterface {
+    COURSE_PLAN_INTERNAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "CPN-001", "Course-plan internal error occurred");
+
+    private final HttpStatus httpStatus;
+    private final String errorCode;
+    private final String errorLog;
+
+    CoursePlanException(HttpStatus httpStatus, String errorCode, String errorLog) {
+        this.httpStatus = httpStatus;
+        this.errorCode = errorCode;
+        this.errorLog = errorLog;
+    }
+
+    @Override
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    @Override
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    @Override
+    public String getMessage() {
+        return errorLog;
+    }
+}
