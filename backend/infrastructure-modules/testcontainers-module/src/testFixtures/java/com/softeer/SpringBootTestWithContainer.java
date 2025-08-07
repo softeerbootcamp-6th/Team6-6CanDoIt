587ad0b6f9bb4dcaeffa364693d71a562c58308e
naver0504 <qortmdwls1234@naver.com>
2025-08-07 18:12:04 +0900

    feat: TestContainer를 사용한 통합 테스트 SpringBootTestWithContainer 어노테이션 추가

---
+++ backend/infrastructure-modules/testcontainers-module/src/testFixtures/java/com/softeer/SpringBootTestWithContainer.java
@@ -0,0 +1,21 @@
+package com.softeer;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest
+@Testcontainers
+@ActiveProfiles("test")
+@ContextConfiguration(initializers = TestcontainersConfiguration.class)
+public @interface SpringBootTestWithContainer {
+}
