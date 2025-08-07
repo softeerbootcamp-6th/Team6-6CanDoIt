8ae2c2813cf441c423d893ec668b7be75e789f30
Park Jong Seong <pjsc0903@gmail.com>
2025-08-07 09:58:26 +0900

    :recycle: / #56 - Heading, CommontText에서 컴포넌트를 반환하는 함수를 위쪽으로 설정

--- frontend/src/components/atoms/Heading/Heading.tsx
+++ frontend/src/components/atoms/Heading/Heading.tsx
@@ -2,22 +2,14 @@ import type { ReactNode } from 'react';
 import { css } from '@emotion/react';
 import { theme } from '../../../theme/theme.ts';
 
+type FontSizeType = keyof typeof theme.typography.fontSize;
+type HeadingTagType = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+
 interface PropsState {
     HeadingTag: HeadingTagType;
     fontSize?: FontSizeType;
     children: string | ReactNode;
 }
-
-type FontSizeType = keyof typeof theme.typography.fontSize;
-type HeadingTagType = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
-
-const { typography, colors } = theme;
-
-const createHeadingStyle = (fontSize: FontSizeType) => css`
-    color: ${colors.grey[100]};
-    font-weight: ${typography.fontWeight.bold};
-    font-size: ${typography.fontSize[fontSize]};
-`;
 
 function Heading(props: PropsState) {
     const { HeadingTag, fontSize, children } = props;
@@ -54,3 +46,11 @@ export function LabelHeading(props: PropsState) {
         <Heading HeadingTag={HeadingTag} fontSize='label' children={children} />
     );
 }
+
+const { typography, colors } = theme;
+
+const createHeadingStyle = (fontSize: FontSizeType) => css`
+    color: ${colors.grey[100]};
+    font-weight: ${typography.fontWeight.bold};
+    font-size: ${typography.fontSize[fontSize]};
+`;
--- frontend/src/components/atoms/Text/CommonText.tsx
+++ frontend/src/components/atoms/Text/CommonText.tsx
@@ -1,6 +1,10 @@
 import { theme } from '../../../theme/theme.ts';
 import { createTextStyle } from './utils.ts';
 
+type TextTagType = 'span' | 'p';
+type FontSizeType = keyof typeof theme.typography.fontSize;
+type FontWeightType = keyof typeof theme.typography.fontWeight;
+
 interface PropsState {
     TextTag: TextTagType;
     fontSize?: string;
@@ -8,10 +12,6 @@ interface PropsState {
     color?: string;
     children: React.ReactNode;
 }
-
-type TextTagType = 'span' | 'p';
-type FontSizeType = keyof typeof theme.typography.fontSize;
-type FontWeightType = keyof typeof theme.typography.fontWeight;
 
 export default function CommonText(props: PropsState) {
     const { TextTag, color, fontSize, fontWeight, children } = props;
