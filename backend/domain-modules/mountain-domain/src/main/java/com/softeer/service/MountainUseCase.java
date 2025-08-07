3747e14ff38e8457ec7babaa7d7d3095d80d186e
Haemulramen <apple11078@gmail.com>
2025-08-07 14:24:19 +0900

    feat: Adapter 호출 분리 및 등산로 목록 조회 메서드 추가

--- backend/domain-modules/mountain-domain/src/main/java/com/softeer/service/MountainUseCase.java
+++ backend/domain-modules/mountain-domain/src/main/java/com/softeer/service/MountainUseCase.java
@@ -1,5 +1,6 @@
 package com.softeer.service;
 
+import com.softeer.domain.Course;
 import com.softeer.domain.CoursePlan;
 import com.softeer.domain.Mountain;
 
@@ -7,7 +8,11 @@ import java.time.LocalDate;
 import java.util.List;
 
 public interface MountainUseCase {
+    // Course
     CoursePlan getCoursePlan(long courseId, LocalDate date);
+    List<Course> getCoursesByMountainId(long mountainId);
+
+    // Mountain
     List<Mountain> getMountains();
     Mountain getMountainById(long id);
 }
--- backend/domain-modules/mountain-domain/src/main/java/com/softeer/service/impl/MountainUseCaseImpl.java
+++ backend/domain-modules/mountain-domain/src/main/java/com/softeer/service/impl/MountainUseCaseImpl.java
@@ -1,7 +1,9 @@
 package com.softeer.service.impl;
 
+import com.softeer.domain.Course;
 import com.softeer.domain.CoursePlan;
 import com.softeer.domain.Mountain;
+import com.softeer.repository.CourseAdapter;
 import com.softeer.repository.MountainAdapter;
 import com.softeer.service.MountainUseCase;
 import lombok.RequiredArgsConstructor;
@@ -15,10 +17,16 @@ import java.util.List;
 public class MountainUseCaseImpl implements MountainUseCase {
 
     private final MountainAdapter mountainAdapter;
+    private final CourseAdapter courseAdapter;
 
     @Override
     public CoursePlan getCoursePlan(long courseId, LocalDate date) {
-        return mountainAdapter.findCoursePlanByIdAndDate(courseId, date);
+        return courseAdapter.findCoursePlanByIdAndDate(courseId, date);
+    }
+
+    @Override
+    public List<Course> getCoursesByMountainId(long mountainId) {
+        return courseAdapter.findCoursesByMountainId(mountainId);
     }
 
     @Override
