f73449bab2730b69220aefc66eecfffd274387d7
Haemulramen <apple11078@gmail.com>
2025-08-08 00:05:39 +0900

    feat: ShortTimePeriod 필드 타입 변경 (String -> int)

--- backend/common-modules/util/src/main/java/com/softeer/time/ShortTimePeriod.java
+++ backend/common-modules/util/src/main/java/com/softeer/time/ShortTimePeriod.java
@@ -3,24 +3,24 @@ package com.softeer.time;
 import java.time.LocalTime;
 
 public enum ShortTimePeriod {
-    BEFORE_TWO(LocalTime.of(0, 0), LocalTime.of(2, 10), "2300"),
-    TWO(LocalTime.of(2,10), LocalTime.of(5,10), "0200"),
-    FIVE(LocalTime.of(5, 10), LocalTime.of(8, 10), "0500"),
-    EIGHT(LocalTime.of(8, 10), LocalTime.of(11, 10), "0800"),
-    ELEVEN(LocalTime.of(11, 10), LocalTime.of(14, 10), "1100"),
-    FOURTEEN(LocalTime.of(14, 10), LocalTime.of(17, 10), "1400"),
-    SEVENTEEN(LocalTime.of(17, 10), LocalTime.of(20, 10), "1700"),
-    TWENTY(LocalTime.of(20, 10), LocalTime.of(23, 10), "2000"),
-    TWENTY_THREE(LocalTime.of(23, 10), LocalTime.of(23, 59), "2300");
+    BEFORE_TWO(LocalTime.MIN, LocalTime.of(2, 10), 23),
+    TWO(LocalTime.of(2,10), LocalTime.of(5,10), 2),
+    FIVE(LocalTime.of(5, 10), LocalTime.of(8, 10), 5),
+    EIGHT(LocalTime.of(8, 10), LocalTime.of(11, 10), 8),
+    ELEVEN(LocalTime.of(11, 10), LocalTime.of(14, 10), 11),
+    FOURTEEN(LocalTime.of(14, 10), LocalTime.of(17, 10), 14),
+    SEVENTEEN(LocalTime.of(17, 10), LocalTime.of(20, 10), 17),
+    TWENTY(LocalTime.of(20, 10), LocalTime.of(23, 10), 20),
+    TWENTY_THREE(LocalTime.of(23, 10), LocalTime.MAX, 23);
 
     private final LocalTime minTime;
     private final LocalTime maxTime;
-    private final String baseTime;
+    private final int baseHour;
 
-    ShortTimePeriod(LocalTime minTime, LocalTime maxTime, String baseTime) {
+    ShortTimePeriod(LocalTime minTime, LocalTime maxTime, int baseHour) {
         this.minTime = minTime;
         this.maxTime = maxTime;
-        this.baseTime = baseTime;
+        this.baseHour = baseHour;
     }
 
     private boolean contains(LocalTime time) {
@@ -37,7 +37,7 @@ public enum ShortTimePeriod {
         return TWENTY_THREE;
     }
 
-    public String getBaseTime() {
-        return baseTime;
+    public int getBaseHour() {
+        return baseHour;
     }
 }
