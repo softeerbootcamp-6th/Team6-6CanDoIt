0b85179ca1206de17fd3b61bcdae231c0c8ee51f
bobob0311 <qweasdzxc0311@naver.com>
2025-08-08 18:21:56 +0900

    💄 - div태그 래퍼 추가

--- frontend/src/components/molecules/MultiLocationTemperature/MultiLocationTemperature.tsx
+++ frontend/src/components/molecules/MultiLocationTemperature/MultiLocationTemperature.tsx
@@ -15,7 +15,7 @@ export default function MultiLocationTemperature({
     separator = ' | ',
 }: MultiLocationProps) {
     return (
-        <>
+        <div>
             {data.map(({ location, temperature }, idx) => (
                 <LocationTemperatureItem
                     location={location}
@@ -24,7 +24,7 @@ export default function MultiLocationTemperature({
                     separator={separator}
                 />
             ))}
-        </>
+        </div>
     );
 }
 
