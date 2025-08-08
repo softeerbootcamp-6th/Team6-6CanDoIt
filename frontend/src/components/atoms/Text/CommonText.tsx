2cfd2a1b70156b07f1511d1ca54e8337974dd8d6
bobob0311 <qweasdzxc0311@naver.com>
2025-08-08 18:18:22 +0900

    ✨  - commonText 글자 나열할 수 있는 props 추가

--- frontend/src/components/atoms/Text/CommonText.tsx
+++ frontend/src/components/atoms/Text/CommonText.tsx
@@ -10,12 +10,16 @@ interface PropsState {
     fontWeight?: FontWeightType;
     color?: string;
     children: React.ReactNode;
+    flexColoumn?: boolean;
 }
 
 export default function CommonText(props: PropsState) {
-    const { TextTag, color, fontSize, fontWeight, children } = props;
+    const { TextTag, color, fontSize, fontWeight, children, flexColoumn } =
+        props;
 
     const textStyle = css`
+        ${flexColoumn &&
+        'display: flex; flex-direction: column; align-items: center;'}
         ${createTextStyle({
             fontSize,
             fontWeight,
