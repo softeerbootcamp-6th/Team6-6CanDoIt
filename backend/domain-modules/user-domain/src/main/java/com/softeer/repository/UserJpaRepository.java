19c0511064fd4ca44a76aeb537f80630cfe46b18
naver0504 <qortmdwls1234@naver.com>
2025-08-11 17:15:18 +0900

    refactor: UserJpaRepository 패키지 이동으로 인한 삭제

--- backend/domain-modules/user-domain/src/main/java/com/softeer/repository/UserJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.softeer.repository;
-
-import com.softeer.entity.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserJpaRepository extends JpaRepository<UserEntity, Long> {
-}
