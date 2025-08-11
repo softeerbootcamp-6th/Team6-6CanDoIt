dbb0d65e190dfe4357394b573784db0b7d3fa53a
naver0504 <qortmdwls1234@naver.com>
2025-08-11 17:14:14 +0900

    feat: UserJpaRepository 생성

---
+++ backend/domain-modules/user-domain/src/main/java/com/softeer/repository/impl/UserJpaRepository.java
@@ -0,0 +1,16 @@
+package com.softeer.repository.impl;
+
+import com.softeer.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface UserJpaRepository extends JpaRepository<UserEntity, Long> {
+
+    @Query("select u from UserEntity u  inner join fetch ImageEntity i on u.imageEntity.id = i.id where u.id = :id")
+    Optional<UserEntity> findById(@Param("id") long id);
+    boolean existsByLoginId(@Param("loginId") String loginId);
+    boolean existsByNickname(@Param("nickname") String nickname);
+}
