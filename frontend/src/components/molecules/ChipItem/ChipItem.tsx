42a17b68227c99135588561b6c4a357f172d2309
Park Jong Seong <pjsc0903@gmail.com>
2025-08-11 23:02:35 +0900

    :sparkles: / #88 - 아이콘과 텍스트를 묶은 래퍼를 molecule에 구현

---
+++ frontend/src/components/molecules/ChipItem/ChipItem.tsx
@@ -0,0 +1,29 @@
+import TextWithIcon from '../TextWithIcon/TextWithIcon.tsx';
+import { css } from '@emotion/react';
+import { theme } from '../../../theme/theme.ts';
+
+export default function ChipItem({
+    text,
+    iconName,
+}: {
+    text: string;
+    iconName: string;
+}) {
+    return (
+        <div css={chipStyle}>
+            <TextWithIcon text={text} iconName={iconName} color='grey-80' />
+        </div>
+    );
+}
+
+const { colors } = theme;
+
+const chipStyle = css`
+    width: max-content;
+    height: max-content;
+    text-align: center;
+    padding: 0.5rem 1.25rem;
+    background-color: ${colors.grey[30]};
+    border-radius: 62.4375rem;
+    cursor: pointer;
+`;
