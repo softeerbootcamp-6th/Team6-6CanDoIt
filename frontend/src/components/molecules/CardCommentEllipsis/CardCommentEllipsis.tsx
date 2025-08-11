a6151ac906b36601aeff033e7c28a77960719c04
Park Jong Seong <pjsc0903@gmail.com>
2025-08-11 22:42:27 +0900

    :sparkles: / #87 - 제보카드의 앞면에 글을 두 줄이 넘어가면 말줌임을 통해 보여지도록 CardCommentEllipsis를 molecule에 구현

---
+++ frontend/src/components/molecules/CardCommentEllipsis/CardCommentEllipsis.tsx
@@ -0,0 +1,29 @@
+import { css } from '@emotion/react';
+import { theme } from '../../../theme/theme.ts';
+
+interface prosState {
+    children: React.ReactNode;
+}
+
+export default function CardCommentEllipsis(props: prosState) {
+    const { children } = props;
+    return <p css={commentStyle}>{children}</p>;
+}
+
+const { colors, typography } = theme;
+
+const commentStyle = css`
+    width: 15.625rem;
+    height: 2.875rem;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    word-break: break-all;
+
+    line-height: 150%;
+    font-size: ${typography.fontSize.caption};
+    font-weight: ${typography.fontWeight.medium};
+    color: ${colors.grey[50]};
+`;
