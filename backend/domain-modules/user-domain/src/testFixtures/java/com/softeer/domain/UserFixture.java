3a8222fad916ef02852164ac3809f81fa80de9dd
naver0504 <qortmdwls1234@naver.com>
2025-08-11 14:40:38 +0900

    test: User DomainFixture 추가

---
+++ backend/domain-modules/user-domain/src/testFixtures/java/com/softeer/domain/UserFixture.java
@@ -0,0 +1,57 @@
+package com.softeer.domain;
+
+import com.softeer.entity.Role;
+
+public class UserFixture {
+
+    public static UserFixtureBuilder builder() {
+        return new UserFixtureBuilder();
+    }
+
+    public static User createDefault() {
+        return builder().build();
+    }
+
+    public static class UserFixtureBuilder {
+        private long id = 0L;
+        private String nickname = "testNickname";
+        private String email = "testEmail";
+        private String password = "testPassword";
+        private Role role = Role.NORMAL;
+        private String imageUrl = "testImageUrl";
+
+        public UserFixtureBuilder id(long id) {
+            this.id = id;
+            return this;
+        }
+
+        public UserFixtureBuilder nickname(String nickname) {
+            this.nickname = nickname;
+            return this;
+        }
+
+        public UserFixtureBuilder email(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public UserFixtureBuilder password(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public UserFixtureBuilder role(Role role) {
+            this.role = role;
+            return this;
+        }
+
+        public UserFixtureBuilder imageUrl(String imageUrl) {
+            this.imageUrl = imageUrl;
+            return this;
+        }
+
+        public User build() {
+            return new User(id, nickname, email, password, role, imageUrl);
+        }
+    }
+}
