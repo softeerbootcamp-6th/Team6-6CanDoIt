9327801d438a4f62f572231fa7aa238a775fb75a
naver0504 <qortmdwls1234@naver.com>
2025-08-11 10:58:34 +0900

    feat: 코스 관련 예보 조회하는 메소드 인터페이스 추가

--- backend/domain-modules/forecast-domain/src/main/java/com/softeer/service/ForecastUseCase.java
+++ backend/domain-modules/forecast-domain/src/main/java/com/softeer/service/ForecastUseCase.java
@@ -2,9 +2,13 @@ package com.softeer.service;
 
 import com.softeer.domain.Forecast;
 import com.softeer.domain.Grid;
+import com.softeer.time.HikingTime;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ForecastUseCase {
-    List<Forecast> findForecastsFromNow(Grid grid);
+    List<Forecast> findForecastsFromStartDateTime(Grid grid, LocalDateTime startDateTime);
+    CourseForecast findForecastsByHikingTime(Grid grid, HikingTime hikingTime);
+    record CourseForecast(Forecast startForecast, Forecast arrivalForecast, Forecast descentForecast) {}
 }
