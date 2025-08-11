364ee0ce8f1424205bcec36f7fc0ff8d44cf8574
Park Jong Seong <pjsc0903@gmail.com>
2025-08-11 23:07:43 +0900

    :adhesive_bandage: / #85 - SearchBar를 메인페이지, 제보페이지, 안전페이지에 맞게 렌더링되도록 수정

--- frontend/src/pages/MainPage/MainPage.tsx
+++ frontend/src/pages/MainPage/MainPage.tsx
@@ -27,23 +27,16 @@ export default function MainPage() {
     );
 }
 
-const mountainCourseData = [
-    {
-        title: '산',
-        options: ['설악산', '한라산', '지리산'],
-    },
-    {
-        title: '코스',
-        options: ['코스1', '코스2', '코스3'],
-    },
-];
+const mountainData = ['설악산', '한라산', '지리산'];
+const courseData = ['코스1', '코스2', '코스3'];
 
 const searchBarProps = {
     searchBarTitle: '어디 날씨를 확인해볼까요?',
     searchBarMessage: '를 오르는',
-    isHomePage: true,
-    mountainCourseData,
-};
+    pageName: 'main',
+    mountainData,
+    courseData,
+} as const;
 
 const backgroundStyle = css`
     position: absolute;
