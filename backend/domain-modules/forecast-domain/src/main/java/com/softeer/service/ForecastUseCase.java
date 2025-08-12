8e543a7a647726172cdf4d374e37577a319eb3cf
naver0504 <qortmdwls1234@naver.com>
2025-08-12 17:39:29 +0900

    feat: 지표면 예보와 정상 예보 반환하는 findForecastWeatherCondition 구현

--- backend/domain-modules/forecast-domain/src/main/java/com/softeer/service/ForecastUseCase.java
+++ backend/domain-modules/forecast-domain/src/main/java/com/softeer/service/ForecastUseCase.java
@@ -2,6 +2,8 @@ package com.softeer.service;
 
 import com.softeer.domain.Forecast;
 import com.softeer.domain.Grid;
+import com.softeer.entity.enums.PrecipitationType;
+import com.softeer.entity.enums.Sky;
 import com.softeer.time.HikingTime;
 
 import java.time.LocalDateTime;
@@ -9,6 +11,18 @@ import java.util.List;
 
 public interface ForecastUseCase {
     List<Forecast> findForecastsFromStartDateTime(Grid grid, LocalDateTime startDateTime);
+
     CourseForecast findForecastsByHikingTime(Grid startGrid, Grid destinationGrid, HikingTime hikingTime);
+
     record CourseForecast(Forecast startForecast, Forecast arrivalForecast, Forecast descentForecast) {}
+
+    WeatherCondition findForecastWeatherCondition(Grid grid, LocalDateTime dateTime);
+
+    record WeatherCondition(PrecipitationType precipitationType, Sky sky, double surfaceTemperature, double topTemperature, String hikingActivityStatus) {
+        public WeatherCondition(Forecast surfaceForecast, Forecast topForecast) {
+            this(topForecast.precipitationCondition().precipitationType(), topForecast.skyCondition().sky(),
+                    surfaceForecast.temperatureCondition().temperature(), topForecast.temperatureCondition().temperature(), topForecast.getHikingActivityStatus()
+            );
+        }
+    }
 }
