7e739cf37c9424e902ba71e58e235854678c4147
Haemulramen <apple11078@gmail.com>
2025-08-12 14:31:20 +0900

    feat: 위도, 경도로 grid 객체를 조회하는 로직 작성

--- backend/domain-modules/grid-domain/src/main/java/com/softeer/repository/GridJpaRepository.java
+++ backend/domain-modules/grid-domain/src/main/java/com/softeer/repository/GridJpaRepository.java
@@ -3,5 +3,9 @@ package com.softeer.repository;
 import com.softeer.entity.GridEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface GridJpaRepository extends JpaRepository<GridEntity, Integer> {
+
+    Optional<GridEntity> findByXAndY(int x, int y);
 }
