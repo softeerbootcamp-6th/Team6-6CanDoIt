a30aa4fd510e6ceb2f39c1e34f39974615509629
bobob0311 <qweasdzxc0311@naver.com>
2025-08-12 14:22:03 +0900

    ✨ / #89 - loginFrom과 제목 등 로그인 페이지를 구성하는 template 구현

---
+++ frontend/src/components/templates/Login/LoginTemplate.tsx
@@ -0,0 +1,41 @@
+import CommonText from '../../atoms/Text/CommonText';
+import mainImg from '../../../assets/mainImg.png';
+import LoginForm from '../../organisms/Form/LoginForm';
+import { css } from '@emotion/react';
+import { theme } from '../../../theme/theme';
+
+const { colors, typography } = theme;
+
+export default function LoginTemplate() {
+    return (
+        <>
+            <h1>
+                <img width={156} height={40} src={mainImg}></img>
+            </h1>
+            <CommonText
+                TextTag='p'
+                color='grey-70'
+                fontSize='body'
+                fontWeight='bold'
+            >
+                국립공원을 오르는 오늘을 위한 기상 예보
+            </CommonText>
+            <LoginForm />
+            <a href='#' css={LinkStyles}>
+                이메일 가입
+            </a>
+        </>
+    );
+}
+
+const LinkStyles = css`
+    color: ${colors.grey[100]};
+    text-align: center;
+    font-family: Pretendard;
+    font-size: ${typography.fontSize.caption};
+    font-weight: ${typography.fontWeight.regular};
+    text-decoration-line: underline;
+    text-decoration-style: solid;
+    text-underline-position: from-font;
+    cursor: pointer;
+`;
