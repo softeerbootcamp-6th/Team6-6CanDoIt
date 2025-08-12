a90f2c04cad1e5b60c8a51741e34d5fe54b83ab5
naver0504 <qortmdwls1234@naver.com>
2025-08-12 18:05:35 +0900

    feat: MountainCardResponse에 산 설명 필드 추가

--- backend/application-modules/api-server/src/main/java/com/softeer/dto/response/card/MountainCardResponse.java
+++ backend/application-modules/api-server/src/main/java/com/softeer/dto/response/card/MountainCardResponse.java
@@ -3,8 +3,8 @@ package com.softeer.dto.response.card;
 import com.softeer.domain.Mountain;
 import com.softeer.service.ForecastUseCase;
 
-public record MountainCardResponse(String mountainName, String mountainImageUrl, WeatherMetric weatherMetric) {
+public record MountainCardResponse(String mountainName, String mountainImageUrl, String mountainDescription, WeatherMetric weatherMetric) {
     public MountainCardResponse(Mountain mountain, ForecastUseCase.WeatherCondition weatherCondition) {
-        this(mountain.name(), mountain.imageUrl(), new WeatherMetric(weatherCondition));
+        this(mountain.name(), mountain.imageUrl(), mountain.description(), new WeatherMetric(weatherCondition));
     }
 }
