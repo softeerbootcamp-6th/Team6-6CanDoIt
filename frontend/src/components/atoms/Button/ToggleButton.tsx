7a9cb1b7dc393739b0a0d94b5aabd3f5e0641b46
bobob0311 <qweasdzxc0311@naver.com>
2025-08-14 00:31:59 +0900

    ♻️ - module.scss emotion으로 변경

--- frontend/src/components/atoms/Button/ToggleButton.tsx
+++ frontend/src/components/atoms/Button/ToggleButton.tsx
@@ -1,9 +1,32 @@
-import styles from './ToggleButton.module.scss';
+import { css } from '@emotion/react';
+import { theme } from '../../../theme/theme';
 
 export default function ToggleButton() {
     return (
-        <div className={styles.toggleButtonWrapper}>
-            <div className={styles.circle}></div>
+        <div css={buttonWrapperStyles}>
+            <div css={circleStyles}></div>
         </div>
     );
 }
+
+const { colors } = theme;
+
+const buttonWrapperStyles = css`
+    display: flex;
+    align-items: center;
+
+    padding-left: 0.1rem;
+    width: 3.2rem;
+    height: 2rem;
+
+    background-color: ${colors.grey[80]};
+    border-radius: 1rem;
+`;
+
+const circleStyles = css`
+    width: 1.8rem;
+    height: 1.8rem;
+    border-radius: 100%;
+
+    background-color: ${colors.grey[40]};
+`;
