6fa2916d4acd97ea85c58d51698a43461fde4a14
naver0504 <qortmdwls1234@naver.com>
2025-08-13 15:51:31 +0900

    feat: login 시 existsByLoginIdAndPassword -> findByLoginIdAndPassword로 메소드 변경

--- backend/domain-modules/user-domain/src/main/java/com/softeer/repository/UserAdapter.java
+++ backend/domain-modules/user-domain/src/main/java/com/softeer/repository/UserAdapter.java
@@ -1,8 +1,12 @@
 package com.softeer.repository;
 
+import com.softeer.entity.UserEntity;
+
+import java.util.Optional;
+
 public interface UserAdapter {
     boolean existsByLoginId(String loginId);
     boolean existsByNickname(String nickname);
     void save(String loginId, String nickname, String password);
-    boolean existsByLoginIdAndPassword(String loginId, String password);
+    Optional<UserEntity> findByLoginIdAndPassword(String loginId, String password);
 }
