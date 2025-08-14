0c5520516b88be66ab84adc37eabfd59b6dc63a4
Haemulramen <apple11078@gmail.com>
2025-08-15 01:01:08 +0900

    feat: MountainForecastApiCaller 클래스 로직 간소화 및 메서드 제거

--- backend/infrastructure-modules/kma-adapter-module/src/main/java/com/softeer/mountain/MountainForecastApiCaller.java
+++ backend/infrastructure-modules/kma-adapter-module/src/main/java/com/softeer/mountain/MountainForecastApiCaller.java
@@ -1,45 +1,18 @@
 package com.softeer.mountain;
 
-import com.softeer.AbstractKmaApiCaller;
-import com.softeer.KmaApiCaller;
+import com.softeer.common.AbstractKmaApiCaller;
 import com.softeer.config.ForecastApiType;
 import com.softeer.mountain.dto.MountainForecastApiResponse;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
-import java.util.List;
+public class MountainForecastApiCaller extends AbstractKmaApiCaller<MountainForecastApiResponse> {
 
-@Component
-public class MountainForecastApiCaller
-        extends AbstractKmaApiCaller<MountainForecastApiCaller.Request, MountainForecastApiResponse>
-        implements KmaApiCaller<MountainForecastApiCaller.Request, MountainForecastApiResponse> {
-
-    public record Request(
-            int mountainNum,
-            String base_date,
-            String base_time
-    ) {}
-
-    public MountainForecastApiCaller(
-            @Qualifier("mountainForecastApiWebClient") RestClient restClient,
-            @Value("${kma.api.key.mountain}") String apiKey) {
-        super(restClient, apiKey, MountainForecastApiResponse.class, ForecastApiType.MOUNTAIN);
+    public MountainForecastApiCaller(RestClient restClient) {
+        super(restClient, ForecastApiType.MOUNTAIN);
     }
 
     @Override
     protected String getApiPath() {
         return "/api/typ08/getMountainWeather";
-    }
-
-    @Override
-    public Class<Request> getRequestType() {
-        return Request.class;
-    }
-
-    @Override
-    public List<MountainForecastApiResponse> call(Request request) {
-        return super.call(request);
     }
 }
