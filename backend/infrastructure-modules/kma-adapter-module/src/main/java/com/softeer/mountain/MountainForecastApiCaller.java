5569339dea7b78b889607241581cc338c8033dd7
Haemulramen <apple11078@gmail.com>
2025-08-14 16:36:03 +0900

    feat: MountainForecastApiCaller 클래스 추가 (산악 단기 예보 api 호출 및 response 파싱 반환)

---
+++ backend/infrastructure-modules/kma-adapter-module/src/main/java/com/softeer/mountain/MountainForecastApiCaller.java
@@ -0,0 +1,40 @@
+package com.softeer.mountain;
+
+import com.softeer.AbstractKmaApiCaller;
+import com.softeer.KmaApiCaller;
+import com.softeer.config.ForecastApiType;
+import com.softeer.mountain.dto.MountainForecastApiResponse;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+import java.util.List;
+
+@Component
+public class MountainForecastApiCaller
+        extends AbstractKmaApiCaller<MountainForecastApiCaller.Request, MountainForecastApiResponse>
+        implements KmaApiCaller<MountainForecastApiCaller.Request, MountainForecastApiResponse> {
+
+    public record Request(
+            int mountainNum,
+            String base_date,
+            String base_time
+    ) {}
+
+    public MountainForecastApiCaller(
+            @Qualifier("mountainForecastApiWebClient") RestClient restClient,
+            @Value("${kma.api.key.mountain}") String apiKey) {
+        super(restClient, apiKey, MountainForecastApiResponse.class, ForecastApiType.MOUNTAIN);
+    }
+
+    @Override
+    protected String getApiPath() {
+        return "/api/typ08/getMountainWeather";
+    }
+
+    @Override
+    public List<MountainForecastApiResponse> call(Request request) {
+        return super.call(request);
+    }
+}
