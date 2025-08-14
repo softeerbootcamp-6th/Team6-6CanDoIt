20e94de5f0c5ee3e7c3977551fd257830c11c992
bobob0311 <qweasdzxc0311@naver.com>
2025-08-15 01:19:58 +0900

    ✨ /#144 - validationRule 배열 추가 및 기존 valid 변수들 삭제

--- frontend/src/components/organisms/Register/RegisterForm.tsx
+++ frontend/src/components/organisms/Register/RegisterForm.tsx
@@ -2,48 +2,59 @@ import { css } from '@emotion/react';
 import FormButton from '../../atoms/Button/FormButton.tsx';
 import TextInputWithIcon from '../../molecules/Input/TextInputWithIcon.tsx';
 import RegisterCheckBoxes from '../Register/RegisterCheckBoxes.tsx';
-import { iconButtonHandler } from './utils.ts';
+import { iconButtonHandler, validHandler } from './utils.ts';
+
+interface ValidationRule {
+    check: (value: string) => boolean;
+    message: string;
+}
+
+const inputFields = [
+    {
+        id: 'email-input',
+        icon: 'x-circle',
+        label: '이메일 주소',
+        type: 'text',
+        iconAriaLabel: '이메일 지우기',
+        onIconClick: iconButtonHandler.clearTextByRef,
+        validations: [
+            {
+                check: validHandler.isValidIdLength,
+                message: '아이디는 6자 이상, 20자 이하이어야 합니다.',
+            },
+            {
+                check: validHandler.isAlphaNumeric,
+                message: '아이디는 영어와 숫자만 사용할 수 있습니다.',
+            },
+        ] as ValidationRule[],
+    },
+    {
+        id: 'password-input',
+        icon: 'eye-off',
+        label: '비밀번호',
+        type: 'password',
+        iconAriaLabel: '비밀번호 보기',
+        onIconClick: iconButtonHandler.togglePasswordVisibility,
+    },
+    {
+        id: 'password-confirm-input',
+        icon: 'eye-off',
+        label: '비밀번호 확인',
+        type: 'password',
+        iconAriaLabel: '비밀번호 보기',
+        onIconClick: iconButtonHandler.togglePasswordVisibility,
+    },
+    {
+        id: 'nickname-input',
+        icon: 'x-circle',
+        label: '닉네임',
+        type: 'text',
+        iconAriaLabel: '닉네임 지우기',
+        onIconClick: iconButtonHandler.clearTextByRef,
+    },
+] as const;
 
 export default function RegisterForm() {
-    const inputFields = [
-        {
-            id: 'email-input',
-            icon: 'x-circle',
-            label: '이메일 주소',
-            type: 'text',
-            iconAriaLabel: '이메일 지우기',
-            onIconClick: iconButtonHandler.clearTextByRef,
-            validationMessage: '이메일 형식이 올바르지 않습니다.',
-        },
-        {
-            id: 'password-input',
-            icon: 'eye-off',
-            label: '비밀번호',
-            type: 'password',
-            iconAriaLabel: '비밀번호 보기',
-            onIconClick: iconButtonHandler.togglePasswordVisibility,
-            validationMessage: '비밀번호 형식이 올바르지 않습니다.',
-        },
-        {
-            id: 'password-confirm-input',
-            icon: 'eye-off',
-            label: '비밀번호 확인',
-            type: 'password',
-            iconAriaLabel: '비밀번호 보기',
-            onIconClick: iconButtonHandler.togglePasswordVisibility,
-            validationMessage: '비밀번호가 일치하지 않습니다.',
-        },
-        {
-            id: 'nickname-input',
-            icon: 'x-circle',
-            label: '닉네임',
-            type: 'text',
-            iconAriaLabel: '닉네임 지우기',
-            onIconClick: iconButtonHandler.clearTextByRef,
-            validationMessage: '닉네임을 입력해주세요.',
-        },
-    ] as const;
-
     return (
         <form css={formWrapperStyles}>
             <TextInputWithIcon {...inputFields[0]} />
