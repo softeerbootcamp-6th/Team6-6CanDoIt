58dfeb9043435528c173dd4664723a7dc3a3e453
Haemulramen <apple11078@gmail.com>
2025-08-17 01:17:27 +0900

    feat: ShortForecastApiCaller 수정 및 UltraForecastApiCaller 클래스 구현

--- backend/infrastructure-modules/kma-adapter-module/src/main/java/com/softeer/shortterm/ShortForecastApiCaller.java
+++ backend/infrastructure-modules/kma-adapter-module/src/main/java/com/softeer/shortterm/ShortForecastApiCaller.java
@@ -1,52 +1,17 @@
 package com.softeer.shortterm;
 
-import com.softeer.common.AbstractKmaApiCaller;
-import com.softeer.common.ApiRequest;
-import com.softeer.config.ForecastApiType;
-import com.softeer.shortterm.dto.response.ShortForecastBody;
-import com.softeer.shortterm.dto.response.ShortForecastItem;
-import com.softeer.shortterm.dto.response.ShortForecastApiResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.client.RestClient;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import java.net.URI;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
 
 @Slf4j
-public class ShortForecastApiCaller extends AbstractKmaApiCaller<ShortForecastItem> {
+public class ShortForecastApiCaller extends AbstractShortForecastApiCaller {
 
     public ShortForecastApiCaller(RestClient restClient) {
-        super(restClient, ForecastApiType.SHORT_TERM);
+        super(restClient);
     }
 
     @Override
     protected String getApiPath() {
         return "/1360000/VilageFcstInfoService_2.0/getVilageFcst";
-    }
-
-    @Override
-    public <T extends ApiRequest> List<ShortForecastItem> call(T request) {
-        URI uri = UriComponentsBuilder
-                .fromPath(getApiPath())
-                .query(request.queryString())
-                .build(true)
-                .toUri();
-
-        ShortForecastApiResponse response = restClient.get()
-                .uri(uri)
-                .retrieve()
-                .body(ShortForecastApiResponse.class);
-
-        if (response != null && response.getHeader() != null && "00".equals(response.getHeader().resultCode())) {
-            return Optional.of(response)
-                    .map(ShortForecastApiResponse::getBody)
-                    .map(ShortForecastBody::getItems)
-                    .orElse(Collections.emptyList());
-        } else {
-            return Collections.emptyList();
-        }
     }
 }
