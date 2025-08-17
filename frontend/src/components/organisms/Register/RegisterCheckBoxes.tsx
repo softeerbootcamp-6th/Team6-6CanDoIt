ff04cbe5d64d41ec2ddf1f7ea8262e5eadd4930e
bobob0311 <qweasdzxc0311@naver.com>
2025-08-17 19:50:23 +0900

    ✨ / #144 - 내용보기 버튼 및 상태 추가

--- frontend/src/components/organisms/Register/RegisterCheckBoxes.tsx
+++ frontend/src/components/organisms/Register/RegisterCheckBoxes.tsx
@@ -2,12 +2,13 @@ import { css } from '@emotion/react';
 import CheckBox from '../../atoms/Form/CheckBox.tsx';
 import type { ColorValueType } from '../../../types/themeTypes';
 import { useState } from 'react';
+import { theme } from '../../../theme/theme.ts';
 
 interface CheckBoxItem {
     id: string;
     text: string;
     required: boolean;
-    ModalText?: string;
+    Modal?: React.ReactNode;
 }
 
 export default function RegisterCheckBoxes() {
@@ -18,6 +19,7 @@ export default function RegisterCheckBoxes() {
 
     const [checkedItems, setCheckedItems] = useState(initialCheckedItems);
     const [allChecked, setAllChecked] = useState(false);
+    const [openModalId, setOpenModalId] = useState<string | null>(null);
 
     const handleAllCheck = () => {
         const newState = !allChecked;
@@ -38,6 +40,10 @@ export default function RegisterCheckBoxes() {
         setAllChecked(allAreChecked);
     };
 
+    const handleModalToggle = (id: string) => {
+        setOpenModalId((prev) => (prev === id ? null : id));
+    };
+
     return (
         <>
             <CheckBox
@@ -47,13 +53,25 @@ export default function RegisterCheckBoxes() {
             />
             <div css={checkBoxStyles}>
                 {checkBoxes.map((box) => (
-                    <CheckBox
-                        key={box.id}
-                        id={box.id}
-                        text={box.text}
-                        onChange={() => handleIndividualCheck(box.id)}
-                        checked={checkedItems[box.id]}
-                    />
+                    <div css={checkLineWrapper}>
+                        <CheckBox
+                            key={box.id}
+                            id={box.id}
+                            text={box.text}
+                            onChange={() => handleIndividualCheck(box.id)}
+                            checked={checkedItems[box.id]}
+                        />
+                        {box.Modal && (
+                            <button
+                                type='button'
+                                css={buttonStyles}
+                                onClick={() => handleModalToggle(box.id)}
+                            >
+                                내용 보기
+                            </button>
+                        )}
+                        {openModalId === box.id && box.Modal}
+                    </div>
                 ))}
             </div>
         </>
@@ -74,13 +92,13 @@ const checkBoxes: CheckBoxItem[] = [
         id: 'terms',
         text: '[필수] 이용약관 동의',
         required: true,
-        ModalText: '가나다라마바사',
+        Modal: <div>check</div>,
     },
     {
         id: 'privacy',
         text: '[선택] 개인정보 마케팅 활용 동의',
         required: false,
-        ModalText: '가나다라마바사',
+        Modal: '[내용보기]',
     },
     {
         id: 'event',
@@ -89,8 +107,24 @@ const checkBoxes: CheckBoxItem[] = [
     },
 ];
 
+const { colors } = theme;
+
 const checkBoxStyles = css`
     display: flex;
     flex-direction: column;
     gap: 0.4rem;
+`;
+
+const checkLineWrapper = css`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+const buttonStyles = css`
+    all: unset;
+    color: ${colors.grey[70]};
+    font-size: 0.75rem;
+    cursor: pointer;
+    border-bottom: 1px solid ${colors.grey[70]};
 `;
