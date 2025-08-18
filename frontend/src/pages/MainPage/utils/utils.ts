87b762b9ba52b36fbcf07b48d67895a47fd2116d
Park Jong Seong <pjsc0903@gmail.com>
2025-08-18 17:51:41 +0900

    :recycle: / #146 - 사용하지 않는 createStartLoading함수 삭제

--- frontend/src/pages/MainPage/utils/utils.ts
+++ frontend/src/pages/MainPage/utils/utils.ts
@@ -1,21 +1,6 @@
 import type { Dispatch, SetStateAction } from 'react';
-import type { SelectedMountainData } from '../../../types/mountainTypes';
 
-export function createStartLoading({
-    setSelectedMountain,
-    setIsLoading,
-}: {
-    setSelectedMountain: Dispatch<SetStateAction<SelectedMountainData | null>>;
-    setIsLoading: Dispatch<SetStateAction<boolean>>;
-}) {
-    return (mountainInfo: SelectedMountainData) => {
-        setSelectedMountain(mountainInfo);
-        setIsLoading(true);
-        setTimeout(() => setIsLoading(false), 3000);
-    };
-}
-
-export function createFormValidChange({
+export function formValidChange({
     setIsOpen,
 }: {
     setIsOpen: Dispatch<SetStateAction<boolean>>;
