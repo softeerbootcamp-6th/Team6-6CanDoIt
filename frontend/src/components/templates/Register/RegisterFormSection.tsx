461a995ac12336ae6af7237b31cc62ef6b20adbf
bobob0311 <qweasdzxc0311@naver.com>
2025-08-19 02:40:32 +0900

    ✨ / #143 - 회원가입 api 연동

--- frontend/src/components/templates/Register/RegisterFormSection.tsx
+++ frontend/src/components/templates/Register/RegisterFormSection.tsx
@@ -2,12 +2,46 @@ import { css } from '@emotion/react';
 import RegisterHeader from '../../molecules/Register/RegisterHeader.tsx';
 import RegisterForm from '../../organisms/Register/RegisterForm.tsx';
 import { useRef } from 'react';
+import useApiMutation from '../../../hooks/useApiMutation.ts';
+
+interface SignUpRequest {
+    nickname: string;
+    loginId: string;
+    password: string;
+}
+
+interface SignUpResponse {
+    message: string;
+}
 
 export default function RegisterFormSection() {
     const idRef = useRef<null | HTMLInputElement>(null);
     const passwordRef = useRef<null | HTMLInputElement>(null);
     const passwordConfirmRef = useRef<null | HTMLInputElement>(null);
     const nicknameRef = useRef<null | HTMLInputElement>(null);
+
+    const mutation = useApiMutation<SignUpRequest, SignUpResponse>(
+        '/user/sign-up',
+        'POST',
+        {
+            onSuccess: (data) => {
+                if (typeof data === 'string') {
+                    alert(`서버 응답: ${data}`);
+                } else {
+                    alert(`회원가입 성공: ${data.message}`);
+                }
+            },
+            onError: (error: Error) => alert(`회원가입 실패: ${error.message}`),
+        },
+    );
+
+    const clickRegisterHandler = () => {
+        mutation.mutate({
+            nickname: nicknameRef.current?.value ?? '',
+            loginId: idRef.current?.value ?? '',
+            password: passwordRef.current?.value ?? '',
+        });
+    };
 
     return (
         <div css={wrapperStyles}>
@@ -19,6 +53,7 @@ export default function RegisterFormSection() {
                     passwordConfirmRef,
                     nicknameRef,
                 }}
+                onClickRegister={clickRegisterHandler}
             />
         </div>
     );
