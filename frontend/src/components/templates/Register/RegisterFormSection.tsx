4c1ab29bbe42f579fe5a1e280d5c71bdf971f62e
bobob0311 <qweasdzxc0311@naver.com>
2025-08-18 18:46:02 +0900

    ✨  / #143 - 회원가입 인풋 ref들 template에서 관리

--- frontend/src/components/templates/Register/RegisterFormSection.tsx
+++ frontend/src/components/templates/Register/RegisterFormSection.tsx
@@ -1,12 +1,25 @@
 import { css } from '@emotion/react';
 import RegisterHeader from '../../molecules/Register/RegisterHeader.tsx';
 import RegisterForm from '../../organisms/Register/RegisterForm.tsx';
+import { useRef } from 'react';
 
 export default function RegisterFormSection() {
+    const idRef = useRef<null | HTMLInputElement>(null);
+    const passwordRef = useRef<null | HTMLInputElement>(null);
+    const passwordConfirmRef = useRef<null | HTMLInputElement>(null);
+    const nicknameRef = useRef<null | HTMLInputElement>(null);
+
     return (
         <div css={wrapperStyles}>
             <RegisterHeader />
-            <RegisterForm />
+            <RegisterForm
+                refs={{
+                    idRef,
+                    passwordRef,
+                    passwordConfirmRef,
+                    nicknameRef,
+                }}
+            />
         </div>
     );
 }
