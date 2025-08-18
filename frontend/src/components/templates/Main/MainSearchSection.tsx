3dc1cd698c3659f2c989d5542d1db0e11671a393
Park Jong Seong <pjsc0903@gmail.com>
2025-08-18 17:42:41 +0900

    :recycle: / #146 - MainSearchSection에서 전체 산 리스트 데이터 상태를 관리하도록 수정 및 선택한 산, 코스, 요일을 통해 forecast페이지로 이동하도록 수정

--- frontend/src/components/templates/Main/MainSearchSection.tsx
+++ frontend/src/components/templates/Main/MainSearchSection.tsx
@@ -1,19 +1,17 @@
 import SearchBar from '../../organisms/Common/SearchBar.tsx';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import type {
     MountainCourse,
     MountainData,
-    SelectedMountainData,
 } from '../../../types/mountainTypes';
 import {
     createHandleSubmit,
     refactorCoursesDataToOptions,
     refactorMountainDataToOptions,
 } from './utils/utils.ts';
+import { useNavigate } from 'react-router-dom';
 
 interface PropsState {
-    mountainsData: MountainData[];
-    onSearchClick: (data: SelectedMountainData) => void;
     onFormValidChange: (isValid: boolean) => void;
 }
 
@@ -23,19 +21,23 @@ interface Option {
 }
 
 export default function MainSearchSection(props: PropsState) {
-    const { mountainsData, onSearchClick, onFormValidChange } = props;
-    const [coursesData, setCoursesData] =
-        useState<MountainCourse[]>(initCoursesData);
+    const { onFormValidChange } = props;
+    const [coursesData, setCoursesData] = useState<MountainCourse[]>([]);
+    const [mountainsData, setMountainsData] = useState<MountainData[]>([]);
+    useEffect(() => {
+        setMountainsData(MountainsData);
+        setCoursesData(initCoursesData);
+    }, []);
 
     const mountainOptions: Option[] =
         refactorMountainDataToOptions(mountainsData);
 
     const courseOptions: Option[] = refactorCoursesDataToOptions(coursesData);
 
+    const navigate = useNavigate();
     const handleSubmit = createHandleSubmit({
-        mountainsData,
-        onSearchClick,
         onFormValidChange,
+        navigate,
     });
     const handleMountainChange = (data: Option) => {
         // 선택한 산에 해당하는 코스 데이터를 가져올 예정, setCoursesData를 통해 상태 업데이트
@@ -48,6 +50,7 @@ export default function MainSearchSection(props: PropsState) {
             pageName='main'
             mountainOptions={mountainOptions}
             courseOptions={courseOptions}
+            weekdayOptions={weekdayData}
             onSubmit={handleSubmit}
             onMountainChange={handleMountainChange}
         />
@@ -55,3 +58,84 @@ export default function MainSearchSection(props: PropsState) {
 }
 
 const initCoursesData = [{ courseId: '1', courseName: '산을 선택해주세요' }];
+
+const MountainsData = [
+    {
+        mountainId: '1',
+        mountainName: '태백산',
+        mountainImageUrl: 'https://cdn.example.com/images/taebaek.png',
+        mountainDescription: '한겨울 설경이 아름다운 산입니다.',
+        weatherMetric: {
+            precipitationType: 'NONE',
+            sky: 'SUNNY',
+            surfaceTemperature: 23.5,
+            topTemperature: 18.2,
+        },
+    },
+    {
+        mountainId: '2',
+        mountainName: '지리산',
+        mountainImageUrl: 'https://cdn.example.com/images/jiri.png',
+        mountainDescription: '한국에서 두 번째로 높은 산입니다.',
+        weatherMetric: {
+            precipitationType: 'RAIN',
+            sky: 'OVERCAST',
+            surfaceTemperature: 20.1,
+            topTemperature: 15.3,
+        },
+    },
+    {
+        mountainId: '3',
+        mountainName: '백두산',
+        mountainImageUrl: 'https://cdn.example.com/images/taebaek.png',
+        mountainDescription: '한겨울 설경이 아름다운 산입니다.',
+        weatherMetric: {
+            precipitationType: 'NONE',
+            sky: 'SUNNY',
+            surfaceTemperature: 23.5,
+            topTemperature: 18.2,
+        },
+    },
+    {
+        mountainId: '4',
+        mountainName: '한라산',
+        mountainImageUrl: 'https://cdn.example.com/images/jiri.png',
+        mountainDescription: '한국에서 두 번째로 높은 산입니다.',
+        weatherMetric: {
+            precipitationType: 'RAIN',
+            sky: 'OVERCAST',
+            surfaceTemperature: 20.1,
+            topTemperature: 15.3,
+        },
+    },
+    {
+        mountainId: '5',
+        mountainName: '설악산',
+        mountainImageUrl: 'https://cdn.example.com/images/taebaek.png',
+        mountainDescription: '한겨울 설경이 아름다운 산입니다.',
+        weatherMetric: {
+            precipitationType: 'NONE',
+            sky: 'SUNNY',
+            surfaceTemperature: 23.5,
+            topTemperature: 18.2,
+        },
+    },
+    {
+        mountainId: '6',
+        mountainName: '가야산',
+        mountainImageUrl: 'https://cdn.example.com/images/jiri.png',
+        mountainDescription: '한국에서 두 번째로 높은 산입니다.',
+        weatherMetric: {
+            precipitationType: 'RAIN',
+            sky: 'OVERCAST',
+            surfaceTemperature: 20.1,
+            topTemperature: 15.3,
+        },
+    },
+];
+
+const weekdayData = [
+    { id: 'today', name: '오늘' },
+    { id: 'tomorrow', name: '내일' },
+    { id: 'dayaftertomorrow', name: '글피' },
+];
