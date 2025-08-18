1741d5ff8a3c4b50b4296ca32faf13071bf30d0c
bobob0311 <qweasdzxc0311@naver.com>
2025-08-19 02:31:29 +0900

    ✨ / #143 - QueryClientProvider로 감싸기

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import LoginPage from './pages/LoginPage/LoginPage';
 import MainPage from './pages/MainPage/MainPage';
 import ForecastPage from './pages/ForecastPage/ForecastPage';
@@ -9,21 +10,25 @@ import Header from './components/organisms/Common/Header';
 import RegisterPage from './pages/RegisterPage/RegisterPage';
 import MyPage from './pages/MyPage/MyPage';
 
+const queryClient = new QueryClient();
+
 function App() {
     return (
-        <BrowserRouter>
-            <Header />
-            <Routes>
-                <Route path='/' element={<MainPage />}></Route>
-                <Route path='/forecast' element={<ForecastPage />}></Route>
-                <Route path='/alert' element={<AlertPage />}></Route>
-                <Route path='/login' element={<LoginPage />}></Route>
-                <Route path='/register' element={<RegisterPage />}></Route>
-                <Route path='/report' element={<ReportPage />}></Route>
-                <Route path='/myPage' element={<MyPage />}></Route>
-                {/* <Route path='*' element={<NotFound />}></Route> */}
-            </Routes>
-        </BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <Header />
+                <Routes>
+                    <Route path='/' element={<MainPage />}></Route>
+                    <Route path='/forecast' element={<ForecastPage />}></Route>
+                    <Route path='/alert' element={<AlertPage />}></Route>
+                    <Route path='/login' element={<LoginPage />}></Route>
+                    <Route path='/register' element={<RegisterPage />}></Route>
+                    <Route path='/report' element={<ReportPage />}></Route>
+                    <Route path='/myPage' element={<MyPage />}></Route>
+                    {/* <Route path='*' element={<NotFound />}></Route> */}
+                </Routes>
+            </BrowserRouter>
+        </QueryClientProvider>
     );
 }
 
