c911ca29b44d68b161b75b828f387a0e228b7794
bobob0311 <qweasdzxc0311@naver.com>
2025-08-20 01:47:08 +0900

    ♻️ / #184 - api형식에 맞춘 더미데이터 추가

--- frontend/src/components/templates/Forecast/SummaryInfoSection.tsx
+++ frontend/src/components/templates/Forecast/SummaryInfoSection.tsx
@@ -1,13 +1,28 @@
 import MountainInfoPreview from '../../molecules/Forecast/MountainInfoPreview.tsx';
-import { WeatherIndexLight } from '../../atoms/Text/WeatherIndex.tsx';
 import { css } from '@emotion/react';
 import ScrollIndicator from '../../molecules/Forecast/ScrollIndicator.tsx';
+
+const dummyData = {
+    courseImageUrl: 'https://cdn.example.com/images/course01.png',
+    totalDuration: 2.5,
+    totalDistance: 4.8,
+    weatherMetric: {
+        precipitationType: 'NONE',
+        sky: 'CLOUDY',
+        surfaceTemperature: 21.3,
+        topTemperature: 17.0,
+    },
+    hikingActivityStatus: '좋음',
+};
 
 export default function SummaryInfoSection() {
     return (
         <div css={wrapperStyles}>
-            <MountainInfoPreview time='0401' dist={5} />
-            <WeatherIndexLight type='매우좋음' />
+            <MountainInfoPreview
+                totalDuration={dummyData.totalDuration}
+                totalDistance={dummyData.totalDistance}
+                courseImageUrl={dummyData.courseImageUrl}
+            />
             <ScrollIndicator />
         </div>
     );
@@ -17,6 +32,8 @@ const wrapperStyles = css`
     display: flex;
     flex-direction: column;
     align-items: center;
-    height: calc(100dvh);
+    height: calc(100dvh - 12.5rem);
     justify-content: space-between;
+    box-sizing: border-box;
+    padding-bottom: 3rem;
 `;
