fa263a13a4e2f27d41e2e223940bb6b1410e719f
Haemulramen <apple11078@gmail.com>
2025-08-19 12:07:37 +0900

    feat: RedisContainerContainer 수정 (버전 7 변경 및 host, port, username, password, ssl 설정)

--- backend/infrastructure-modules/testcontainers-module/src/testFixtures/java/com/softeer/RedisContainerContainer.java
+++ backend/infrastructure-modules/testcontainers-module/src/testFixtures/java/com/softeer/RedisContainerContainer.java
@@ -9,7 +9,7 @@ import org.testcontainers.utility.DockerImageName;
 
 public class RedisContainerContainer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
 
-    private static final String REDIS_DOCKER_IMAGE = "redis:5.0.3-alpine";
+    private static final String REDIS_DOCKER_IMAGE = "redis:7.0.3-alpine";
 
     @Container
     static GenericContainer<?> LETTUCE_CONTAINER = new GenericContainer<>(DockerImageName.parse(REDIS_DOCKER_IMAGE))
@@ -23,8 +23,11 @@ public class RedisContainerContainer implements ApplicationContextInitializer<Co
     @Override
     public void initialize(ConfigurableApplicationContext applicationContext) {
         TestPropertyValues.of(
-                "spring.redis.host=localhost",
-                "spring.redis.port=" + LETTUCE_CONTAINER.getFirstMappedPort()
+                "spring.redis.host=" + LETTUCE_CONTAINER.getHost(),
+                "spring.redis.port=" + LETTUCE_CONTAINER.getFirstMappedPort(),
+                "spring.redis.username=",          // redis 5.x는 ACL username 미지원 → 빈 값
+                "spring.redis.password=",          // 패스워드 미사용
+                "spring.redis.ssl=false"           // SSL 미사용
         ).applyTo(applicationContext.getEnvironment());
     }
 }
