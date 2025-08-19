7812500b5b8c62520f2b689822a819ae0ba61f20
naver0504 <qortmdwls1234@naver.com>
2025-08-19 14:24:40 +0900

    feat: Getter 추가

--- backend/domain-modules/interaction-domain/src/main/java/com/softeer/entity/keyword/EtceteraKeywordEntity.java
+++ backend/domain-modules/interaction-domain/src/main/java/com/softeer/entity/keyword/EtceteraKeywordEntity.java
@@ -1,16 +1,17 @@
 package com.softeer.entity.keyword;
 
 import com.softeer.entity.enums.EtceteraKeyword;
-import com.softeer.entity.enums.WeatherKeyword;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "etcetera_keyword")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class EtceteraKeywordEntity {
 
     @Id
--- backend/domain-modules/interaction-domain/src/main/java/com/softeer/entity/keyword/WeatherKeywordEntity.java
+++ backend/domain-modules/interaction-domain/src/main/java/com/softeer/entity/keyword/WeatherKeywordEntity.java
@@ -4,12 +4,14 @@ import com.softeer.entity.enums.WeatherKeyword;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "weather_keyword")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class WeatherKeywordEntity {
 
     @Id
