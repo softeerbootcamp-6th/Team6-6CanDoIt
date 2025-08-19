aac3bc776457fa25bc8f979b612fcc8c5a3b6433
Haemulramen <apple11078@gmail.com>
2025-08-19 13:43:22 +0900

    feat: ShortForecastWriterSupporter 클래스 추가 (Forecast 데이터 jdbc 방식 저장)

---
+++ backend/application-modules/batch-server/src/main/java/com/softeer/batch/forecast/shortterm/writersupporter/ShortForecastWriterSupporter.java
@@ -0,0 +1,46 @@
+package com.softeer.batch.forecast.shortterm.writersupporter;
+
+import com.softeer.domain.Forecast;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Component;
+
+@Component
+@StepScope
+@RequiredArgsConstructor
+public class ShortForecastWriterSupporter {
+
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    private static final String INSERT_FORECAST =
+            "INSERT INTO forecast (temperature, precipitation, sky, humidity, precipitation_type, wind_dir, wind_speed, date_time, type, precipitation_probability, snow_accumulation, grid_id) " +
+                    "VALUES (:temperature, :precipitation, :sky, :humidity, :precipitationType, :windDir, :windSpeed, :dateTime, :type, :precipitationProbability, :snowAccumulation, :gridId) " +
+                    "ON DUPLICATE KEY UPDATE " +
+                    "temperature = VALUES(temperature), precipitation = VALUES(precipitation), sky = VALUES(sky), humidity = VALUES(humidity), precipitation_type = VALUES(precipitation_type), " +
+                    "wind_dir = VALUES(wind_dir), wind_speed = VALUES(wind_speed), type = VALUES(type), precipitation_probability = VALUES(precipitation_probability), snow_accumulation = VALUES(snow_accumulation)";
+
+    public void batchUpdateForecast(SqlParameterSource[] params) {
+        if (params != null && params.length > 0) {
+            namedParameterJdbcTemplate.batchUpdate(INSERT_FORECAST, params);
+        }
+    }
+
+    public MapSqlParameterSource mapForecastToSqlParams(Forecast hourly, int gridId) {
+        return new MapSqlParameterSource()
+                .addValue("temperature", hourly.temperatureCondition().temperature())
+                .addValue("precipitation", hourly.precipitationCondition().precipitation())
+                .addValue("sky", hourly.skyCondition().sky().name())
+                .addValue("humidity", hourly.humidityCondition().humidity())
+                .addValue("precipitationType", hourly.precipitationCondition().precipitationType().name())
+                .addValue("windDir", hourly.windCondition().direction().name())
+                .addValue("windSpeed", hourly.windCondition().windSpeed())
+                .addValue("dateTime", hourly.dateTime())
+                .addValue("type", hourly.forecastType().name())
+                .addValue("precipitationProbability", hourly.precipitationCondition().precipitationProbability())
+                .addValue("snowAccumulation", hourly.precipitationCondition().snowAccumulation())
+                .addValue("gridId", gridId);
+    }
+}
