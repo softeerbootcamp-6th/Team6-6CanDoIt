38a9d85ddb368fd3968bcd4c6bee97861af8ecf8
naver0504 <qortmdwls1234@naver.com>
2025-08-19 14:26:39 +0900

    feat: 키워드 조회 로직 구현

--- backend/domain-modules/interaction-domain/src/main/java/com/softeer/repository/report/impl/ReportQueryAdapterImpl.java
+++ backend/domain-modules/interaction-domain/src/main/java/com/softeer/repository/report/impl/ReportQueryAdapterImpl.java
@@ -2,7 +2,13 @@ package com.softeer.repository.report.impl;
 
 import com.softeer.domain.Report;
 import com.softeer.entity.enums.ReportType;
+import com.softeer.entity.keyword.EtceteraKeywordEntity;
+import com.softeer.entity.keyword.RainKeywordEntity;
+import com.softeer.entity.keyword.WeatherKeywordEntity;
 import com.softeer.repository.report.ReportQueryAdapter;
+import com.softeer.repository.report.impl.jpa.EtceteraKeywordJpaRepository;
+import com.softeer.repository.report.impl.jpa.RainKeywordJpaRepository;
+import com.softeer.repository.report.impl.jpa.WeatherKeywordJpaRepository;
 import com.softeer.repository.support.filter.KeywordFilter;
 import com.softeer.repository.support.pageable.ReportPageable;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +22,10 @@ public class ReportQueryAdapterImpl implements ReportQueryAdapter {
 
 
     private final ReportQuerydslRepository reportQuerydslRepository;
+    private final WeatherKeywordJpaRepository weatherKeywordJpaRepository;
+    private final RainKeywordJpaRepository rainKeywordJpaRepository;
+    private final EtceteraKeywordJpaRepository etceteraKeywordJpaRepository;
+
 
     @Override
     public List<Report> findReportsByCourseIdAndType(ReportPageable pageable, KeywordFilter keywordFilter, long courseId, ReportType reportType) {
@@ -30,5 +40,20 @@ public class ReportQueryAdapterImpl implements ReportQueryAdapter {
     @Override
     public List<Report> findLikedReports(ReportPageable reportPageable, long userId) {
         return reportQuerydslRepository.findLikedReports(reportPageable, userId);
+    }
+
+    @Override
+    public List<WeatherKeywordEntity> findAllWeatherKeywords() {
+        return weatherKeywordJpaRepository.findAll();
+    }
+
+    @Override
+    public List<RainKeywordEntity> findAllRainKeywords() {
+        return rainKeywordJpaRepository.findAll();
+    }
+
+    @Override
+    public List<EtceteraKeywordEntity> findAllEtceteraKeywords() {
+        return etceteraKeywordJpaRepository.findAll();
     }
 }
--- backend/domain-modules/interaction-domain/src/main/java/com/softeer/service/ReportQueryUseCase.java
+++ backend/domain-modules/interaction-domain/src/main/java/com/softeer/service/ReportQueryUseCase.java
@@ -1,5 +1,6 @@
 package com.softeer.service;
 
+import com.softeer.domain.Keyword;
 import com.softeer.domain.Report;
 import com.softeer.entity.enums.ReportType;
 import com.softeer.repository.support.filter.KeywordFilter;
@@ -12,5 +13,9 @@ public interface ReportQueryUseCase {
                                               long courseId, ReportType reportType);
     List<Report> findMyReports(ReportPageable reportPageable, long userId);
     List<Report> findLikedReports(ReportPageable reportPageable, long userId);
+
+    KeywordGroup findAllKeywords();
+
+    record KeywordGroup(List<Keyword> weatherKeywords, List<Keyword> rainKeywords, List<Keyword> etceteraKeywords) { }
 }
 
