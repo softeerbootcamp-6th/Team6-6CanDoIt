403d110dcce2eee0a4bffcd735d81e536ad5b787
Haemulramen <apple11078@gmail.com>
2025-08-20 11:49:27 +0900

    feat: local 테스트 설정 삭제

--- backend/infrastructure-modules/s3-client-module/src/main/java/com/softeer/config/S3Config.java
+++ backend/infrastructure-modules/s3-client-module/src/main/java/com/softeer/config/S3Config.java
@@ -3,14 +3,9 @@ package com.softeer.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.S3Configuration;
-
-import java.net.URI;
 
 @Configuration
 public class S3Config {
@@ -23,18 +18,9 @@ public class S3Config {
 
     @Bean
     public S3Client s3Client() {
-//        return S3Client.builder()
-//                .region(Region.AP_NORTHEAST_2)
-//                .credentialsProvider(ProfileCredentialsProvider.create())
-//                .build();
         return S3Client.builder()
-                .endpointOverride(URI.create(endpoint)) // LocalStack edge 포트
-                .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(
-                        AwsBasicCredentials.create("test", "test"))) // 더미
-                .serviceConfiguration(S3Configuration.builder()
-                        .pathStyleAccessEnabled(true) // http://endpoint/bucket/key
-                        .build())
+                .region(Region.AP_NORTHEAST_2)
+                .credentialsProvider(ProfileCredentialsProvider.create())
                 .build();
     }
 }
