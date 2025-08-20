128e2304dbe8d81ecd6287bf36f406a802af18ab
Haemulramen <apple11078@gmail.com>
2025-08-20 16:37:42 +0900

    refactor: ReportJpaUseCaseImplTest.java >> 클래스명 변경 >> ReportCommandUseCaseImplTest.java

--- backend/domain-modules/interaction-domain/src/test/java/com/softeer/service/impl/ReportCommandUseCaseImplTest.java
+++ backend/domain-modules/interaction-domain/src/test/java/com/softeer/service/impl/ReportCommandUseCaseImplTest.java
@@ -1,11 +1,11 @@
 package com.softeer.service.impl;
 
-import com.softeer.dto.ReportCreateDto;
 import com.softeer.entity.enums.ReportType;
-import com.softeer.repository.report.ReportJpaAdapter;
+import com.softeer.repository.report.ReportCommandAdapter;
 import com.softeer.repository.report.impl.jpa.ReportEtceteraKeywordRepository;
 import com.softeer.repository.report.impl.jpa.ReportRainKeywordJpaRepository;
 import com.softeer.repository.report.impl.jpa.ReportWeatherKeywordRepository;
+import com.softeer.service.ReportCommandUseCase;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -20,13 +20,13 @@ import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class) // Mockito 기능을 JUnit 5 테스트에 활성화
-class ReportJpaUseCaseImplTest {
+class ReportCommandUseCaseImplTest {
 
     @InjectMocks // 테스트 대상 클래스. @Mock으로 생성된 가짜 객체들이 여기에 주입됩니다.
-    private ReportJpaUseCaseImpl reportJpaUseCase;
+    private ReportCommandUseCaseImpl reportJpaUseCase;
 
     @Mock
-    private ReportJpaAdapter reportJpaAdapter;
+    private ReportCommandAdapter reportCommandAdapter;
 
     @Mock
     private ReportWeatherKeywordRepository reportWeatherKeywordRepository;
@@ -41,7 +41,7 @@ class ReportJpaUseCaseImplTest {
     @DisplayName("모든 키워드를 포함한 신고(Report) 저장 성공 테스트")
     void saveReport_Success_WithAllKeywords() {
         // given
-        ReportCreateDto dto = new ReportCreateDto(
+        ReportCommandUseCase.ReportCreateDto dto = new ReportCommandUseCase.ReportCreateDto(
                 1L,
                 101L,
                 ReportType.SAFE,
@@ -53,13 +53,13 @@ class ReportJpaUseCaseImplTest {
         );
         long expectedReportId = 1L;
 
-        when(reportJpaAdapter.saveReport(any(ReportCreateDto.class))).thenReturn(expectedReportId);
+        when(reportCommandAdapter.saveReport(any(ReportCommandUseCase.ReportCreateDto.class))).thenReturn(expectedReportId);
 
         // when
         reportJpaUseCase.saveReport(dto);
 
         // then
-        verify(reportJpaAdapter, times(1)).saveReport(dto);
+        verify(reportCommandAdapter, times(1)).saveReport(dto);
 
         verify(reportWeatherKeywordRepository, times(1)).saveAll(anyList());
         verify(reportRainKeywordRepository, times(1)).saveAll(anyList());
@@ -70,7 +70,7 @@ class ReportJpaUseCaseImplTest {
     @DisplayName("키워드가 없는 신고(Report) 저장 성공 테스트")
     void saveReport_Success_WithNoKeywords() {
         // given
-        ReportCreateDto dto = new ReportCreateDto(
+        ReportCommandUseCase.ReportCreateDto dto = new ReportCommandUseCase.ReportCreateDto(
                 1L,
                 101L,
                 ReportType.SAFE,
@@ -82,13 +82,13 @@ class ReportJpaUseCaseImplTest {
         );
         long expectedReportId = 2L;
 
-        when(reportJpaAdapter.saveReport(any(ReportCreateDto.class))).thenReturn(expectedReportId);
+        when(reportCommandAdapter.saveReport(any(ReportCommandUseCase.ReportCreateDto.class))).thenReturn(expectedReportId);
 
         // when
         reportJpaUseCase.saveReport(dto);
 
         // then
-        verify(reportJpaAdapter, times(1)).saveReport(dto);
+        verify(reportCommandAdapter, times(1)).saveReport(dto);
 
         verify(reportWeatherKeywordRepository, never()).saveAll(anyList());
         verify(reportRainKeywordRepository, never()).saveAll(anyList());
