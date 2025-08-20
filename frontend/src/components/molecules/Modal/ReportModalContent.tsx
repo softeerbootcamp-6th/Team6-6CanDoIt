19a69e5772539361b1531b3903d636a8a29ebd62
Park Jong Seong <pjsc0903@gmail.com>
2025-08-20 10:15:25 +0900

    :sparkles: / #168 - ReportModalContent에서 선택된 라벨리스트 상태관리

--- frontend/src/components/molecules/Modal/ReportModalContent.tsx
+++ frontend/src/components/molecules/Modal/ReportModalContent.tsx
@@ -3,29 +3,63 @@ import { css } from '@emotion/react';
 import { theme } from '../../../theme/theme.ts';
 import ModalTextInputBox from '../Input/ModalTextInputBox.tsx';
 import ImageInputField from '../Input/ImageInputField.tsx';
+import { useState } from 'react';
+import type {
+    FilterColumn,
+    Keyword,
+    SelectedColumn,
+} from '../../../types/FilterTypes';
 
-interface FilterColumn {
-    title: string;
-    filterLabels: string[];
-}
 interface PropsState {
-    filterColumns: FilterColumn[];
+    filterColumn: FilterColumn;
 }
 
-export default function ReportModalContent({ filterColumns }: PropsState) {
+export default function ReportModalContent({ filterColumn }: PropsState) {
+    const [selectedKeywords, setSelectedKeywords] = useState<SelectedColumn>({
+        weatherKeywords: [],
+        rainKeywords: [],
+        etceteraKeywords: [],
+    });
+
+    const updateSelectedKeywords = (
+        keyword: Keyword,
+        selectedIds: number[],
+    ) => {
+        setSelectedKeywords({
+            ...selectedKeywords,
+            [keyword]: selectedIds,
+        });
+    };
+
     return (
         <div css={reportModalContentStyle}>
             <ImageInputField />
             <div css={rightPaneStyle}>
                 <ModalTextInputBox placeholder='사진과 함께 올릴 설명을 100글자 이내로 작성해주세요.' />
                 <div css={labelButtonsRowStyle}>
-                    {filterColumns?.map((column) => (
+                    {keywords.map((keyword) => (
                         <LabelButtonsColumn
-                            title={column.title}
-                            filterLabels={column.filterLabels}
+                            key={keyword}
+                            keyword={keyword}
+                            filterLabels={filterColumn[keyword]}
+                            selectedIds={selectedKeywords[keyword]}
+                            onSelectionChange={(selectedIds) =>
+                                updateSelectedKeywords(keyword, selectedIds)
+                            }
                         />
                     ))}
                 </div>
+
+                {keywords.map((keyword) =>
+                    selectedKeywords[keyword].map((id) => (
+                        <input
+                            key={`${keyword}-${id}`}
+                            type='hidden'
+                            name={`${keyword}`}
+                            value={id}
+                        />
+                    )),
+                )}
             </div>
         </div>
     );
@@ -52,3 +86,9 @@ const labelButtonsRowStyle = css`
     gap: 1.5rem;
     margin-top: 2.5rem;
 `;
+
+const keywords: Keyword[] = [
+    'weatherKeywords',
+    'rainKeywords',
+    'etceteraKeywords',
+];
