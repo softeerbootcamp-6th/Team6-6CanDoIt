702ae0caaf1876c785d3ea3c04ed50de34bdfc05
Haemulramen <apple11078@gmail.com>
2025-08-20 12:03:30 +0900

    feat: S3 설정, 인증 정보 EC2 Role 호출 처리

--- backend/infrastructure-modules/s3-client-module/src/main/java/com/softeer/config/S3Config.java
+++ backend/infrastructure-modules/s3-client-module/src/main/java/com/softeer/config/S3Config.java
@@ -1,26 +1,17 @@
 package com.softeer.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class S3Config {
 
-    @Value("${cloud.aws.s3.endpoint}")
-    private String endpoint;
-
-    @Value("${cloud.aws.s3.region}")
-    private String region;
-
     @Bean
     public S3Client s3Client() {
         return S3Client.builder()
                 .region(Region.AP_NORTHEAST_2)
-                .credentialsProvider(ProfileCredentialsProvider.create())
                 .build();
     }
 }
