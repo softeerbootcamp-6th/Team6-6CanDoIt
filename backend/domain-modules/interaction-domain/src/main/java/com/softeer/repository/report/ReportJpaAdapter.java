41a4003c6f34c7fbe6a2e858f4fa3eae00aad1af
Haemulramen <apple11078@gmail.com>
2025-08-20 16:36:31 +0900

    refactor: ReportJpaAdapter.java >> 클래스명 변경 >> ReportCommandAdapter

--- backend/domain-modules/interaction-domain/src/main/java/com/softeer/repository/report/ReportJpaAdapter.java
+++
@@ -1,8 +0,0 @@
-package com.softeer.repository.report;
-
-import com.softeer.dto.ReportCreateDto;
-import com.softeer.entity.ReportEntity;
-
-public interface ReportJpaAdapter {
-    long saveReport(ReportCreateDto reportCreateDto);
-}
