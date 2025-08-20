56655296928631667fd8ed3a39e8cad173394bdd
naver0504 <qortmdwls1234@naver.com>
2025-08-20 16:38:44 +0900

    refactor: ForecastWriterSupporter.java  역할 분리 (Forecast, SunTime)

--- backend/application-modules/batch-server/src/main/java/com/softeer/batch/common/writersupporter/ForecastJdbcWriter.java
+++ backend/application-modules/batch-server/src/main/java/com/softeer/batch/common/writersupporter/ForecastJdbcWriter.java
@@ -14,15 +14,7 @@ import java.time.LocalDate;
 @Component
 @StepScope
 @RequiredArgsConstructor
-public class ForecastWriterSupporter {
-
-    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
-
-    private static final String INSERT_SUN_TIME =
-            "INSERT INTO sun_time (sunrise, sunset, date, mountain_id) " +
-                    "VALUES (:sunrise, :sunset, :date, :mountainId) " +
-                    "ON DUPLICATE KEY UPDATE sunrise = VALUES(sunrise), sunset = VALUES(sunset)";
-
+public class ForecastJdbcWriter {
     private static final String INSERT_FORECAST =
             "INSERT INTO forecast (temperature, precipitation, sky, humidity, precipitation_type, wind_dir, wind_speed, date_time, type, precipitation_probability, snow_accumulation, grid_id) " +
                     "VALUES (:temperature, :precipitation, :sky, :humidity, :precipitationType, :windDir, :windSpeed, :dateTime, :type, :precipitationProbability, :snowAccumulation, :gridId) " +
@@ -30,11 +22,16 @@ public class ForecastWriterSupporter {
                     "temperature = VALUES(temperature), precipitation = VALUES(precipitation), sky = VALUES(sky), humidity = VALUES(humidity), precipitation_type = VALUES(precipitation_type), " +
                     "wind_dir = VALUES(wind_dir), wind_speed = VALUES(wind_speed), type = VALUES(type), precipitation_probability = VALUES(precipitation_probability), snow_accumulation = VALUES(snow_accumulation)";
 
-    public void batchUpdateSunTime(SqlParameterSource[] params) {
-        if (params != null && params.length > 0) {
-            namedParameterJdbcTemplate.batchUpdate(INSERT_SUN_TIME, params);
-        }
-    }
+    private static final String INSERT_DAILY_TEMPERATURE =
+            "INSERT INTO daily_temperature (date, highest_temperature, lowest_temperature, grid_id) " +
+                    "VALUES (:date, :highestTemperature, :lowestTemperature, :gridId) " +
+                    "ON DUPLICATE KEY UPDATE " +
+                    "highest_temperature = VALUES(highest_temperature), " +
+                    "lowest_temperature = VALUES(lowest_temperature)";
+
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+
 
     public void batchUpdateForecast(SqlParameterSource[] params) {
         if (params != null && params.length > 0) {
@@ -42,12 +39,10 @@ public class ForecastWriterSupporter {
         }
     }
 
-    public MapSqlParameterSource createSunTimeParams(MountainDailyForecast item) {
-        return new MapSqlParameterSource()
-                        .addValue("sunrise", item.sunTime().sunrise())
-                        .addValue("sunset", item.sunTime().sunset())
-                        .addValue("date", LocalDate.now())
-                        .addValue("mountainId", item.mountainId());
+    public void batchUpdateDailyTemperature(SqlParameterSource[] params) {
+        if (params != null && params.length > 0) {
+            namedParameterJdbcTemplate.batchUpdate(INSERT_DAILY_TEMPERATURE, params);
+        }
     }
 
     public MapSqlParameterSource mapForecastToSqlParams(Forecast hourly, int gridId) {
