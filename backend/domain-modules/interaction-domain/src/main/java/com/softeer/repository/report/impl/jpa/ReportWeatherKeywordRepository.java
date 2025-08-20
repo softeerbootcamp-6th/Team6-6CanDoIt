a1876808c0a98a48a55a1ef47fa14b70feb97652
Haemulramen <apple11078@gmail.com>
2025-08-20 11:21:10 +0900

    feat: Report-Keyword 중간 테이블 repository 생성 (weather, rain, etcetera)

---
+++ backend/domain-modules/interaction-domain/src/main/java/com/softeer/repository/report/impl/jpa/ReportWeatherKeywordRepository.java
@@ -0,0 +1,7 @@
+package com.softeer.repository.report.impl.jpa;
+
+import com.softeer.entity.keyword.ReportWeatherKeywordEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReportWeatherKeywordRepository extends JpaRepository<ReportWeatherKeywordEntity,Integer> {
+}
