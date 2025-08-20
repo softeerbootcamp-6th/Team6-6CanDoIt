93ee64b82bdb089c9702204008fb5f7dfee6cb38
Haemulramen <apple11078@gmail.com>
2025-08-20 18:01:50 +0900

    feat: ReportCommandAdapter 메서드 추가 (existsReportLike, saveReportLike, deleteReportLike)

--- backend/domain-modules/interaction-domain/src/main/java/com/softeer/repository/report/ReportCommandAdapter.java
+++ backend/domain-modules/interaction-domain/src/main/java/com/softeer/repository/report/ReportCommandAdapter.java
@@ -1,7 +1,11 @@
 package com.softeer.repository.report;
 
+import com.softeer.entity.id.ReportLikeId;
 import com.softeer.service.ReportCommandUseCase;
 
 public interface ReportCommandAdapter {
     long saveReport(ReportCommandUseCase.ReportCreateDto reportCreateDto);
+    boolean existsReportLike(ReportLikeId reportLikeId);
+    void saveReportLike(ReportLikeId reportLikeId);
+    void deleteReportLike(ReportLikeId reportLikeId);
 }
