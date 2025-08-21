d1362911e7f101498dd41297b974f0734d899bd1
naver0504 <qortmdwls1234@naver.com>
2025-08-21 18:48:43 +0900

    feat: 프로필 업데이트 로직 구현

---
+++ backend/domain-modules/user-domain/src/main/java/com/softeer/service/impl/ProfileCommandUseCaseImpl.java
@@ -0,0 +1,30 @@
+package com.softeer.service.impl;
+
+import com.softeer.repository.UserAdapter;
+import com.softeer.service.ProfileCommandUseCase;
+import com.softeer.service.check.NickNameChecker;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ProfileCommandUseCaseImpl implements ProfileCommandUseCase {
+
+    private final NickNameChecker nickNameChecker;
+    private final UserAdapter userAdapter;
+
+
+    @Override
+    @Transactional
+    public void updateNickname(long userId, String nickname) {
+        nickNameChecker.check(nickname);
+        userAdapter.updateNickname(userId, nickname);
+    }
+
+    @Override
+    @Transactional
+    public void updateProfileImage(long userId, long imageId) {
+        userAdapter.updateImage(imageId, userId);
+    }
+}
