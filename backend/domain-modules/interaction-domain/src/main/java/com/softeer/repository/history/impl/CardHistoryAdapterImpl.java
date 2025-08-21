4ed6de262d1750bb8899c6a3250bc0a399ebbec8
Haemulramen <apple11078@gmail.com>
2025-08-21 11:39:20 +0900

    feat: CardHistoryAdapter 인터페이스 및 클래스 메서드 추가, 구현 (touchOrCreate -> 엔티티가 있다면 업데이트 없다면 생성)

--- backend/domain-modules/interaction-domain/src/main/java/com/softeer/repository/history/impl/CardHistoryAdapterImpl.java
+++ backend/domain-modules/interaction-domain/src/main/java/com/softeer/repository/history/impl/CardHistoryAdapterImpl.java
@@ -1,11 +1,14 @@
 package com.softeer.repository.history.impl;
 
 import com.softeer.domain.CardHistory;
+import com.softeer.entity.CardHistoryEntity;
 import com.softeer.repository.history.CardHistoryAdapter;
 import com.softeer.repository.support.pageable.CardHistoryPageable;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
@@ -13,10 +16,25 @@ import java.util.List;
 public class CardHistoryAdapterImpl implements CardHistoryAdapter {
 
     private final CardHistoryQuerydslRepository cardHistoryQuerydslRepository;
-
+    private final CardHistoryJpaRepository cardHistoryJpaRepository;
 
     @Override
     public List<CardHistory> findUserCardHistory(long userId, CardHistoryPageable pageable) {
         return  cardHistoryQuerydslRepository.findUserCardHistory(userId, pageable);
+    }
+
+    @Override
+    @Transactional
+    public void touchOrCreate(long userId, long courseId, LocalDateTime forecastDate) {
+        cardHistoryJpaRepository.findByUserIdAndCourseId(userId, courseId)
+                .ifPresentOrElse(
+                        CardHistoryEntity::touch,
+                        () -> cardHistoryJpaRepository.save(CardHistoryEntity.builder()
+                                .userId(userId)
+                                .courseId(courseId)
+                                .forecastDate(forecastDate)
+                                .updatedAt(LocalDateTime.now())
+                                .build())
+                );
     }
 }
