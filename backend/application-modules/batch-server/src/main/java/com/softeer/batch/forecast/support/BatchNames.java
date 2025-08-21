774ac91de0451acd11e02ba7ecf6cde5ae71f8b7
Haemulramen <apple11078@gmail.com>
2025-08-21 23:20:01 +0900

    feat: BatchNames 클래스 추가 (Batch jobs, steps 이름 저장)

---
+++ backend/application-modules/batch-server/src/main/java/com/softeer/batch/forecast/support/BatchNames.java
@@ -0,0 +1,22 @@
+package com.softeer.batch.forecast.support;
+
+public final class BatchNames {
+
+    private BatchNames() {}
+
+    public static final class Jobs {
+        // chained forecast jobs
+        public static final String STARTUP_CHAINED_FORECAST_JOB = "startupChainedForecastJob";
+        public static final String SCHEDULED_CHAINED_FORECAST_JOB = "scheduledChainedForecastJob";
+    }
+
+    public static final class Steps {
+        // mountain forecast steps
+        public static final String STARTUP_MOUNTAIN_FORECAST_STEP = "startupMountainForecastStep";
+        public static final String SCHEDULED_MOUNTAIN_FORECAST_STEP = "scheduledMountainForecastStep";
+
+        // short forecast steps
+        public static final String START_UP_SHORT_FORECAST_STEP = "StartUpShortForecastStep";
+        public static final String SCHEDULED_SHORT_FORECAST_STEP = "ScheduledShortForecastStep";
+    }
+}
