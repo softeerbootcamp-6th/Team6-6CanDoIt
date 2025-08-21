9a2958dfe1a48e8d4a3653506dd105fa42d0ea3b
naver0504 <qortmdwls1234@naver.com>
2025-08-21 14:09:23 +0900

    feat: restClient -> webClient로 변경

--- backend/application-modules/batch-server/src/main/java/com/softeer/batch/common/config/ForecastApiCallerConfig.java
+++ backend/application-modules/batch-server/src/main/java/com/softeer/batch/common/config/ForecastApiCallerConfig.java
@@ -1,47 +1,33 @@
 package com.softeer.batch.common.config;
 
-import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.softeer.mountain.MountainForecastApiCaller;
 import com.softeer.shortterm.ShortForecastApiCaller;
 import com.softeer.shortterm.UltraForecastApiCaller;
-import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestClient;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class ForecastApiCallerConfig {
 
     @Bean
-    @StepScope
-    public XmlMapper xmlMapper() {
-        return new XmlMapper();
-    }
-
-    @Bean
-    @StepScope
     public MountainForecastApiCaller mountainForecastApiCaller(
-            @Qualifier("mountainForecastApiRestClient") RestClient restClient,
-            XmlMapper xmlMapper
+            @Qualifier("mountainForecastApiWebClient") WebClient webClient
     ) {
-        return new MountainForecastApiCaller(restClient,  xmlMapper);
+        return new MountainForecastApiCaller(webClient);
     }
 
     @Bean
-    @StepScope
     public ShortForecastApiCaller shortForecastApiCaller(
-            @Qualifier("kmaPublicApiRestClient") RestClient restClient,
-            XmlMapper xmlMapper
+            @Qualifier("kmaPublicApiWebClient") WebClient webClient
     ) {
-        return new ShortForecastApiCaller(restClient,  xmlMapper);
+        return new ShortForecastApiCaller(webClient);
     }
 
     @Bean
-    @StepScope
-    public UltraForecastApiCaller ultraForecastApiCaller(@Qualifier("kmaPublicApiRestClient") RestClient restClient,
-                                                         XmlMapper xmlMapper
+    public UltraForecastApiCaller ultraForecastApiCaller(@Qualifier("kmaPublicApiRestClient") WebClient webClient
     ) {
-        return new UltraForecastApiCaller(restClient, xmlMapper);
+        return new UltraForecastApiCaller(webClient);
     }
 }
