9898072fe9f1d79a7b7d17b60afb2b8d0a6b850c
naver0504 <qortmdwls1234@naver.com>
2025-08-21 16:33:34 +0900

    feat: TokenBucket 알고리즘을 적용한 ThrottleManager 구현

---
+++ backend/infrastructure-modules/redis-module/src/main/java/com/softeer/throttle/manager/impl/TokenBucketThrottlingManager.java
@@ -0,0 +1,25 @@
+package com.softeer.throttle.manager.impl;
+
+import com.softeer.throttle.BackoffStrategy;
+import com.softeer.throttle.ThrottlingProperties;
+import com.softeer.throttle.manager.AbstractThrottlingManager;
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.distributed.proxy.ProxyManager;
+
+import java.time.Duration;
+
+public class TokenBucketThrottlingManager extends AbstractThrottlingManager {
+
+    public TokenBucketThrottlingManager(ProxyManager<String> proxyManager, ThrottlingProperties properties, BackoffStrategy backoffStrategy) {
+        super(proxyManager, properties, backoffStrategy);
+    }
+
+    @Override
+    protected Bandwidth createBandwidth() {
+        return Bandwidth.builder()
+                .capacity(properties.maxTps())
+                .refillGreedy(currentTps.longValue(), Duration.ofSeconds(1))
+                .initialTokens(0)
+                .build();
+    }
+}
