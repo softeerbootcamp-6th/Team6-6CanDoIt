006ecb3afb897f75a45fbc17fc242e345921ca86
naver0504 <qortmdwls1234@naver.com>
2025-08-21 18:47:50 +0900

    feat: 닉네임 및 이미지 업데이트 쿼리 작성

--- backend/domain-modules/user-domain/src/main/java/com/softeer/repository/impl/UserJpaRepository.java
+++ backend/domain-modules/user-domain/src/main/java/com/softeer/repository/impl/UserJpaRepository.java
@@ -2,18 +2,26 @@ package com.softeer.repository.impl;
 
 import com.softeer.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface UserJpaRepository extends JpaRepository<UserEntity, Long> {
-
-    @Query("select u from UserEntity u  inner join fetch ImageEntity i on u.imageEntity.id = i.id where u.id = :id")
-    Optional<UserEntity> findById(@Param("id") long id);
+    
     boolean existsByLoginId(@Param("loginId") String loginId);
     boolean existsByNickname(@Param("nickname") String nickname);
 
     @Query("select u from UserEntity u where u.loginId = :loginId and u.password = :password")
     Optional<UserEntity> findByLoginIdAndPassword(@Param("loginId") String loginId, @Param("password") String password);
+
+
+    @Modifying
+    @Query("update UserEntity u set u.nickname = :nickname where u.id = :userId")
+    void updateNickname(@Param("userId") long userId, @Param("nickname") String nickname);
+
+    @Modifying
+    @Query("update UserEntity u set u.imageEntity.id = :imageId where u.id = :userId")
+    void updateImage(@Param("userId") long userId, @Param("imageId") long imageId);
 }
