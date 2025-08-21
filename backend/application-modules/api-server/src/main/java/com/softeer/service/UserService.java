56ace029aab2bf19a141b2279a34e8866e551a59
naver0504 <qortmdwls1234@naver.com>
2025-08-21 18:48:05 +0900

    feat: 닉네임 및 이미지 업데이트 로직 구현

--- backend/application-modules/api-server/src/main/java/com/softeer/service/UserService.java
+++ backend/application-modules/api-server/src/main/java/com/softeer/service/UserService.java
@@ -1,5 +1,6 @@
 package com.softeer.service;
 
+import com.softeer.domain.ImageMeta;
 import com.softeer.domain.User;
 import com.softeer.dto.request.SignInRequest;
 import com.softeer.dto.request.SignUpRequest;
@@ -8,12 +9,17 @@ import com.softeer.config.auth.Token;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Service
 @RequiredArgsConstructor
-public class SignService {
+public class UserService {
     private final SignUseCase signUseCase;
     private final JwtGenerator jwtGenerator;
+    private final ProfileCommandUseCase profileCommandUseCase;
+    private final ImageUseCase imageUseCase;
 
     public void existsLoginId(String loginId) {
         signUseCase.checkLoginId(loginId);
@@ -34,5 +40,24 @@ public class SignService {
         User user = signUseCase.signIn(signInCommand);
 
         return jwtGenerator.createAccessToken(user.id(), user.role());
+    }
+
+    public void updateNickname(long userId, String nickname) {
+        profileCommandUseCase.updateNickname(userId, nickname);
+    }
+
+    public void updateImage(long userId, MultipartFile imageFile) {
+
+        long imageId;
+
+        try {
+            imageId = imageUseCase.uploadImage(new ImageMeta(imageFile.getBytes(), imageFile.getName(), imageFile.getContentType()));
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to read image file", e);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to upload image", e);
+        }
+
+        profileCommandUseCase.updateProfileImage(userId, imageId);
     }
 }
