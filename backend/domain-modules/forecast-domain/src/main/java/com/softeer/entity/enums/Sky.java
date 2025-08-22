e195322eec5d752d172ff4846b0a529e1c9101d5
Haemulramen <apple11078@gmail.com>
2025-08-22 10:54:40 +0900

    refactor: Sky displayDescription 응답 변경 (기존: 맑은 하늘, 조금 흐린 하늘, 흐린 하늘. 변경: 화창, 구름 많음, 흐림)

--- backend/domain-modules/forecast-domain/src/main/java/com/softeer/entity/enums/Sky.java
+++ backend/domain-modules/forecast-domain/src/main/java/com/softeer/entity/enums/Sky.java
@@ -4,9 +4,9 @@ import lombok.Getter;
 
 @Getter
 public enum Sky {
-    SUNNY("맑음", "맑은 하늘"),
-    CLOUDY("구름 조금", "조금 흐린 하늘"),
-    OVERCAST("흐림", "흐린 하늘");
+    SUNNY("맑음", "화창"),
+    CLOUDY("구름 조금", "구름 많음"),
+    OVERCAST("흐림", "흐림");
 
     private final String description;
     private final String displayDescription;
