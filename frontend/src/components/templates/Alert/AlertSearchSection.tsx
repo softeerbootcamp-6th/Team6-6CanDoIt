768aef1cd112c93a29da523e9d9717db967a90d7
Park Jong Seong <pjsc0903@gmail.com>
2025-08-23 14:31:46 +0900

    :sparkles: / #229 - 메인, 제보, 예보, 안전정보 페이지의 검색섹션 구현 및 선택된 산, 코스, 요일에 대한 상태를 관리하도록 수정

--- frontend/src/components/templates/Alert/AlertSearchSection.tsx
+++ frontend/src/components/templates/Alert/AlertSearchSection.tsx
@@ -1,14 +1,55 @@
 import SearchBar from '../../organisms/Common/SearchBar';
+import { refactorMountainDataToOptions } from '../Main/utils.ts';
+import useApiQuery from '../../../hooks/useApiQuery.ts';
+import type { MountainData } from '../../../types/mountainTypes';
+import { useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+
+interface Option {
+    id: number;
+    name: string;
+}
 
 export default function AlertSearchSection() {
+    const [searchParams, setSearchParams] = useSearchParams();
+    const mountainId = Number(searchParams.get('mountainid'));
+
+    const [selectedMountainId, setSelectedMountainId] =
+        useState<number>(mountainId);
+
+    const mouuntainChangeHandler = (id: number) => {
+        setSelectedMountainId(id);
+    };
+    const submitHandler = (e: React.FormEvent) => {
+        e.preventDefault();
+
+        const next = new URLSearchParams(searchParams);
+        next.set('mountainid', String(selectedMountainId));
+        setSearchParams(next);
+    };
+
+    const { data: mountainsData } = useApiQuery<MountainData[]>(
+        '/card/mountain',
+        {},
+        {
+            //placeholderData: MountainsData,
+            retry: false,
+        },
+    );
+
+    const mountainOptions: Option[] = refactorMountainDataToOptions(
+        mountainsData ?? [],
+    );
+
     return (
         <SearchBar
-            searchBarTitle='어디 안전정보를 확인해볼까요?'
-            searchBarMessage='의 안전정보'
+            searchBarTitle='어디 안전 정보를 확인해볼까요?'
+            searchBarMessage='의 안전 정보'
             pageName='safety'
-            mountainData={mountainData}
+            mountainOptions={mountainOptions}
+            selectedMountainId={selectedMountainId ?? 0}
+            mountainChangeHandler={mouuntainChangeHandler}
+            onSubmit={submitHandler}
         />
     );
 }
-
-const mountainData = ['설악산', '한라산', '지리산'];
