43368b18f165e0faef71d01be43e49abd0c4a6ad
Haemulramen <apple11078@gmail.com>
2025-08-23 20:49:58 +0900

    feat: MountainForecastRedisWriter.prepareBulkData 메서드 내부 로직 수정 (단기 예보 api의 일 최고, 최저 기온 데이터를 ExecutionContext에서 조회해서 함께 저장)

--- backend/application-modules/batch-server/src/main/java/com/softeer/batch/forecast/mountain/redis/MountainForecastRedisWriter.java
+++ backend/application-modules/batch-server/src/main/java/com/softeer/batch/forecast/mountain/redis/MountainForecastRedisWriter.java
@@ -1,23 +1,27 @@
 package com.softeer.batch.forecast.mountain.redis;
 
+import com.softeer.batch.forecast.chained.context.ExecutionContextKeys;
+import com.softeer.batch.forecast.chained.dto.DailyTemperatureKey;
+import com.softeer.batch.forecast.chained.dto.DailyTemperatureValue;
 import com.softeer.batch.forecast.mountain.dto.MountainDailyForecast;
 import com.softeer.entity.enums.ForecastType;
 import com.softeer.mapper.RecordMapper;
 import com.softeer.scan.RedisKeyGenerator;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
 @Slf4j
 @Component
 @StepScope
-@RequiredArgsConstructor
 public class MountainForecastRedisWriter {
 
     private static final String PREFIX = "forecast";
@@ -25,9 +29,22 @@ public class MountainForecastRedisWriter {
     private final RedisKeyGenerator redisKeyGenerator;
     private final RedisTemplate<String, Object> redisTemplate;
     private final RecordMapper recordMapper;
+    private final Map<DailyTemperatureKey, DailyTemperatureValue> dailyTempMap;
+
+    public MountainForecastRedisWriter(
+            RedisKeyGenerator redisKeyGenerator,
+            RedisTemplate<String, Object> redisTemplate,
+            RecordMapper recordMapper,
+            @Value("#{stepExecutionContext['" + ExecutionContextKeys.DAILY_TEMP_MAP + "']}")
+            Map<DailyTemperatureKey, DailyTemperatureValue> dailyTempMap
+    ) {
+        this.redisKeyGenerator = redisKeyGenerator;
+        this.redisTemplate = redisTemplate;
+        this.recordMapper = recordMapper;
+        this.dailyTempMap = dailyTempMap;
+    }
 
     public void pipelineUpdateMountainForecast(List<? extends MountainDailyForecast> items) {
-        log.info("Starting batch update of {} mountain forecast items to Redis", items.size());
         try {
             Map<byte[], Map<byte[], byte[]>> bulkData = prepareBulkData(items);
             executePipelinedOperations(bulkData);
@@ -41,18 +58,26 @@ public class MountainForecastRedisWriter {
     private Map<byte[], Map<byte[], byte[]>> prepareBulkData(List<? extends MountainDailyForecast> items) {
         Map<byte[], Map<byte[], byte[]>> bulkData = new java.util.HashMap<>();
 
-        items.forEach(item -> {
+        items.forEach(item ->
             item.hourlyForecasts().forEach(forecast -> {
                 try {
+
                     byte[] key = redisKeyGenerator.serializeKey(PREFIX, item.gridId(), ForecastType.MOUNTAIN, forecast.dateTime());
                     Map<byte[], byte[]> value = recordMapper.toByteMap(forecast);
-                    bulkData.put(key, value);
 
+                    DailyTemperatureValue dailyTemperatureValue = dailyTempMap.get(new DailyTemperatureKey(item.gridId(), forecast.dateTime().toLocalDate()));
+
+                    byte[] fieldDailyTemp = "dailyTemperature".getBytes(StandardCharsets.UTF_8);
+                    byte[] serializedDailyTemp = recordMapper.serializeValue(dailyTemperatureValue);
+                    value.keySet().removeIf(k -> Arrays.equals(k, fieldDailyTemp));
+                    value.put(fieldDailyTemp, serializedDailyTemp);
+
+                    bulkData.put(key, value);
                 } catch (Exception e) {
                     log.error("Failed to prepare Redis data for grid ({}, {}) at {} {}", PREFIX, item.gridId(), ForecastType.SHORT, forecast.dateTime());
                 }
-            });
-        });
+            })
+        );
 
         return bulkData;
     }
