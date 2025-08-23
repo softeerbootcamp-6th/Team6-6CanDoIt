7319e19793e9208b3ce777826c3bca437daf23af
bobob0311 <qweasdzxc0311@naver.com>
2025-08-23 17:52:55 +0900

    ✨ / #183 -  최고점일경우 최고점의 고도를 표시

--- frontend/src/components/molecules/Forecast/DetailSideBarHeader.tsx
+++ frontend/src/components/molecules/Forecast/DetailSideBarHeader.tsx
@@ -5,19 +5,41 @@ import Icon from '../../atoms/Icon/Icons';
 interface PropsState {
     onClose: () => void;
     type: string;
+    courseAltitude?: number;
 }
 
-export default function DetailSideBarHeader({ onClose, type }: PropsState) {
+export default function DetailSideBarHeader({
+    onClose,
+    type,
+    courseAltitude,
+}: PropsState) {
     return (
         <div css={topStyles}>
-            <CommonText
-                TextTag='span'
-                fontSize='label'
-                fontWeight='bold'
-                color='greyOpacity-40'
+            <span
+                css={css`
+                    display: flex;
+                    gap: 0.5rem;
+                `}
             >
-                {type}
-            </CommonText>
+                <CommonText
+                    TextTag='span'
+                    fontSize='label'
+                    fontWeight='bold'
+                    color='greyOpacity-40'
+                >
+                    {type}
+                </CommonText>
+                {courseAltitude && (
+                    <CommonText
+                        TextTag='span'
+                        fontSize='label'
+                        fontWeight='medium'
+                        color='greyOpacity-60'
+                    >
+                        {`${courseAltitude}m`}
+                    </CommonText>
+                )}
+            </span>
             <button
                 css={css`
                     all: unset;
