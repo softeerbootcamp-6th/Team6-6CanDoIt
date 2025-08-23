4a59ee8f5651a84469689f2d557d682dd41bac2c
bobob0311 <qweasdzxc0311@naver.com>
2025-08-23 18:18:39 +0900

    ✨ / #183 - 쿼리 파라미터를 사용하여 데이터 페칭 및 interface 정의

--- frontend/src/components/templates/Forecast/SummaryInfoSection.tsx
+++ frontend/src/components/templates/Forecast/SummaryInfoSection.tsx
@@ -1,21 +1,14 @@
 import { css } from '@emotion/react';
-import { useState, useEffect } from 'react';
 import MountainInfoPreview from '../../molecules/Forecast/MountainInfoPreview.tsx';
 import ScrollIndicator from '../../molecules/Forecast/ScrollIndicator.tsx';
-
-const placeholderData = {
-    courseImageUrl: 'https://cdn.example.com/images/course01.png',
-    totalDuration: 2.5,
-    totalDistance: 4.8,
-    sunrise: '09:00:00',
-    sunset: '09:00:00',
-    hikingActivityStatus: '좋음',
-} as const;
+import { summaryInfoSectionData } from '../../../constants/placeholderData.ts';
+import useApiQuery from '../../../hooks/useApiQuery.ts';
+import { useSearchParams } from 'react-router-dom';
 
 interface MountainCourseData {
     courseImageUrl: string;
-    totalDuration: number;
-    totalDistance: number;
+    duration: number;
+    distance: number;
     sunrise: string;
     sunset: string;
     hikingActivityStatus: HikingActivityStatus;
@@ -23,22 +16,29 @@ interface MountainCourseData {
 
 type HikingActivityStatus = '좋음' | '매우좋음' | '나쁨' | '보통';
 
+const placeholderData = summaryInfoSectionData;
+
 export default function SummaryInfoSection() {
-    const [mountainCourseData, setMountainCourseData] =
-        useState<MountainCourseData>(placeholderData);
+    const [searchParams] = useSearchParams();
+    const selectedCourseId = Number(searchParams.get('courseid'));
 
-    useEffect(() => {
-        setMountainCourseData(placeholderData);
-    }, []);
+    const { data: summaryInfoSectionData } = useApiQuery<MountainCourseData>(
+        `/card/mountain/course/${selectedCourseId}`,
+        { dateTime: '2025-08-22T00:00:00' },
+        {
+            placeholderData: placeholderData,
+            retry: false,
+        },
+    );
 
-    const { totalDuration, totalDistance, courseImageUrl, sunrise, sunset } =
-        mountainCourseData;
+    const courseData = summaryInfoSectionData ?? placeholderData;
+    const { duration, distance, courseImageUrl, sunrise, sunset } = courseData;
 
     return (
         <div css={wrapperStyles}>
             <MountainInfoPreview
-                totalDuration={totalDuration}
-                totalDistance={totalDistance}
+                totalDuration={duration}
+                totalDistance={distance}
                 courseImageUrl={courseImageUrl}
                 sunriseTime={sunrise.slice(0, 5)}
                 sunsetTime={sunset.slice(0, 5)}
