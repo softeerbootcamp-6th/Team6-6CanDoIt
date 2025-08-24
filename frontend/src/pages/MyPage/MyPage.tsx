11bafb1ecfcf4f43928b7e1617eda7ce50fec531
bobob0311 <qweasdzxc0311@naver.com>
2025-08-25 03:50:17 +0900

    ✨ / #91 - 최근 본 등산 목록 Card 생성 구현

--- frontend/src/pages/MyPage/MyPage.tsx
+++ frontend/src/pages/MyPage/MyPage.tsx
@@ -1,19 +1,44 @@
 import { css } from '@emotion/react';
 import MyInfoSection from '../../components/templates/MyPage/MyInfoSection';
-import { userInfoData } from '../../constants/placeholderData.ts';
 import RecentClimbSection from '../../components/templates/MyPage/RecentClimbSection.tsx';
 import MyReportSection from '../../components/templates/MyPage/MyReportSection.tsx';
 import MyLikeSection from '../../components/templates/MyPage/MyLikeSection.tsx';
-
-const placeholderUserData = userInfoData;
+import { useState } from 'react';
+import WeatherSummaryCardModal from '../../components/templates/MyPage/WeatherSummaryCard.tsx';
 
 export default function MyPage() {
+    const [isCard, setIsCard] = useState<boolean>(false);
+    const [selectedCourseId, setSelectedCourseId] = useState<number | null>(
+        null,
+    );
+    const [selectedForecastDate, setSelectedForecastDate] = useState<
+        string | null
+    >(null);
+
+    const handleBtnClick = (courseId: number, forecastDate: string) => {
+        setSelectedCourseId(courseId);
+        setSelectedForecastDate(forecastDate);
+        setIsCard(true);
+    };
+
     return (
         <div css={wrapperStyles}>
-            <MyInfoSection userData={placeholderUserData} />
-            <RecentClimbSection />
+            <MyInfoSection />
+            <RecentClimbSection
+                onClick={(courseId, forecastDate) =>
+                    handleBtnClick(courseId, forecastDate)
+                }
+            />
+
             <MyReportSection />
             <MyLikeSection />
+            {isCard && (
+                <WeatherSummaryCardModal
+                    onClose={() => setIsCard(false)}
+                    courseId={selectedCourseId!}
+                    forecastDate={selectedForecastDate!}
+                />
+            )}
         </div>
     );
 }
