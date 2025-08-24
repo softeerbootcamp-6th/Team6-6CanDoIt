a04b61c803c63f31cc8046a36acbbb1ff86fd620
bobob0311 <qweasdzxc0311@naver.com>
2025-08-24 14:59:34 +0900

    ✨ / #91 - Mypage 전체 레이아웃 설정 및 template 호출

--- frontend/src/pages/MyPage/MyPage.tsx
+++ frontend/src/pages/MyPage/MyPage.tsx
@@ -1,6 +1,7 @@
 import { css } from '@emotion/react';
 import MyInfoSection from '../../components/templates/MyPage/MyInfoSection';
 import { userInfoData } from '../../constants/placeholderData.ts';
+import RecentClimbSection from '../../components/templates/MyPage/RecentClimbSection.tsx';
 
 const placeholderUserData = userInfoData;
 
@@ -8,11 +9,15 @@ export default function MyPage() {
     return (
         <div css={wrapperStyles}>
             <MyInfoSection userData={placeholderUserData} />
+            <RecentClimbSection />
         </div>
     );
 }
 
 const wrapperStyles = css`
-    width: 75rem;
+    display: flex;
+    flex-direction: column;
+    min-width: 75rem;
     margin: auto;
+    gap: 3.5rem;
 `;
