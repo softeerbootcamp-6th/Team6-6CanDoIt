71ead92d20e782753aed3677e396060eb66ca056
Park Jong Seong <pjsc0903@gmail.com>
2025-08-25 00:37:16 +0900

    :sparkles: / #242 - 아이콘에 fill props 추가

--- frontend/src/components/atoms/Icon/Icons.tsx
+++ frontend/src/components/atoms/Icon/Icons.tsx
@@ -8,13 +8,22 @@ interface PropsState {
     height: number;
     color: string;
     opacity?: number;
+    fill?: string;
     storkeWidth?: number;
 }
 
 const { colors } = theme;
 
 export default function Icon(props: PropsState) {
-    const { name, opacity, color, width, height, storkeWidth = 2 } = props;
+    const {
+        name,
+        opacity,
+        color,
+        width,
+        height,
+        fill = 'none',
+        storkeWidth = 2,
+    } = props;
 
     const pathData = PATH[name];
     if (!pathData) return null;
@@ -24,7 +33,7 @@ export default function Icon(props: PropsState) {
             width={`${width}rem`}
             height={`${height}rem`}
             viewBox={`0 0 ${width * 16} ${height * 16}`}
-            fill='none'
+            fill={fill}
             xmlns='http://www.w3.org/2000/svg'
         >
             <path
