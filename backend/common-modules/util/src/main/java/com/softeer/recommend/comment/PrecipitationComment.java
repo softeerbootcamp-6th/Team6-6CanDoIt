e57603cb62c3f1157c4fe11cf7c2ad33629f2df9
naver0504 <qortmdwls1234@naver.com>
2025-08-24 21:14:30 +0900

    feat: "1.0mm 미만"도 파싱하도록 기능 추가

--- backend/common-modules/util/src/main/java/com/softeer/recommend/comment/PrecipitationComment.java
+++ backend/common-modules/util/src/main/java/com/softeer/recommend/comment/PrecipitationComment.java
@@ -14,23 +14,22 @@ import static com.softeer.recommend.TypeLevel.*;
 
 public enum PrecipitationComment implements Comment {
 
-
-    LEVEL_0(new TotalLevel(LEVEL_ZERO, PRECIPITATION), new Literal("강수없음"), "등산하기 좋은 날씨에요."),
-    LEVEL_1(new TotalLevel(LEVEL_ONE, PRECIPITATION), new Literal("1mm 미만"), "등산하기 좋은 날씨에요."),
-    LEVEL_2(new TotalLevel(LEVEL_TWO, PRECIPITATION), new MillimeterRange(1.0, 1.5), "바람막이를 챙겨가세요."),
-    LEVEL_3(new TotalLevel(LEVEL_THREE, PRECIPITATION), new MillimeterRange(1.5, 3), "바람이 강하게 부니 등산 시 유의해주세요."),
-    LEVEL_4(new TotalLevel(LEVEL_FOUR, PRECIPITATION), PrecipitationComment::alwaysTrue, "오늘은 등산을 강력히 자제해 주세요."),
+    LEVEL_0(new TotalLevel(LEVEL_ZERO, PRECIPITATION), List.of(new Literal("강수없음")), "등산하기 좋은 날씨에요."),
+    LEVEL_1(new TotalLevel(LEVEL_ONE, PRECIPITATION), List.of(new Literal("1mm 미만"), new Literal("1.0mm 미만")), "등산하기 좋은 날씨에요."),
+    LEVEL_2(new TotalLevel(LEVEL_TWO, PRECIPITATION), List.of(new MillimeterRange(1.0, 1.5)), "바람막이를 챙겨가세요."),
+    LEVEL_3(new TotalLevel(LEVEL_THREE, PRECIPITATION), List.of(new MillimeterRange(1.5, 3)), "바람이 강하게 부니 등산 시 유의해주세요."),
+    LEVEL_4(new TotalLevel(LEVEL_FOUR, PRECIPITATION), List.of(PrecipitationComment::alwaysTrue), "오늘은 등산을 강력히 자제해 주세요."),
     ;
 
     private static final List<PrecipitationComment> precipitationComments = Arrays.asList(values());
 
     private final TotalLevel totalLevel;
-    private final Condition condition;
+    private final List<Condition> conditions;
     private final String comment;
 
-    PrecipitationComment(TotalLevel totalLevel, Condition condition, String comment) {
+    PrecipitationComment(TotalLevel totalLevel, List<Condition> conditions, String comment) {
         this.totalLevel = totalLevel;
-        this.condition = condition;
+        this.conditions = conditions;
         this.comment = comment;
     }
 
@@ -38,16 +37,16 @@ public enum PrecipitationComment implements Comment {
         return true;
     }
 
-
     private boolean isInRange(String precipitation) {
-        return condition.matches(precipitation);
+        return conditions.stream().anyMatch(condition -> condition.matches(precipitation));
     }
 
     public static PrecipitationComment findPrecipitationComment(String precipitation) {
         for (PrecipitationComment precipitationComment : precipitationComments) {
-            if(precipitationComment.isInRange(precipitation)) return precipitationComment;
+            if (precipitationComment.isInRange(precipitation)) {
+                return precipitationComment;
+            }
         }
-
         throw new IllegalArgumentException("정의되지 않은 풍속 범위: " + precipitation);
     }
 
