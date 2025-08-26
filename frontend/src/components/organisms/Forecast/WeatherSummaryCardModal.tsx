b40a1977c8bfe3f9a7a4f0ba8404c338ac2b49e9
bobob0311 <qweasdzxc0311@naver.com>
2025-08-26 23:42:10 +0900

    ♻️ / #280 - LoginRequiredModal로 분리 및 import 정리

--- frontend/src/components/organisms/Forecast/WeatherSummaryCardModal.tsx
+++ frontend/src/components/organisms/Forecast/WeatherSummaryCardModal.tsx
@@ -1,14 +1,16 @@
 import { css } from '@emotion/react';
 import { theme } from '../../../theme/theme.ts';
-import FrontWeatherSummaryCard from './FrontWeatherSummaryCard.tsx';
 import { useState } from 'react';
-import BackWeatherSummaryCard from './BackWeatherSummaryCard.tsx';
-import WeatherSummaryCardHeader from '../../molecules/Forecast/WeatherSummaryCardHeader.tsx';
-import Icon from '../../atoms/Icon/Icons.tsx';
-import useApiMutation from '../../../hooks/useApiMutation.ts';
-import Modal from '../../molecules/Modal/RegisterModal.tsx';
 import { useNavigate } from 'react-router-dom';
-import { useForecastCardData } from '../../../hooks/useForecastCardData.ts';
+
+import useApiMutation from '../../../hooks/useApiMutation.ts';
+import useForecastCardData from '../../../hooks/useForecastCardData.ts';
+
+import Icon from '../../atoms/Icon/Icons.tsx';
+import WeatherSummaryCardHeader from '../../molecules/Forecast/WeatherSummaryCardHeader.tsx';
+import LoginRequiredModal from '../../molecules/Button/LoginRequiredModal.tsx';
+import FrontWeatherSummaryCard from './FrontWeatherSummaryCard.tsx';
+import BackWeatherSummaryCard from './BackWeatherSummaryCard.tsx';
 
 interface Props {
     onClose: () => void;
@@ -98,48 +100,13 @@ export default function WeatherSummaryCardModal({
                 />
             </button>
             {errorMessage && (
-                <Modal
-                    onClose={() => {
-                        setErrorMessage('');
-                    }}
-                >
-                    <div
-                        css={css`
-                            display: flex;
-                            flex-direction: column;
-                            gap: 2rem;
-                            align-items: center;
-                        `}
-                    >
-                        로그인이 필요한 서비스입니다.
-                        <button
-                            css={btnStyles}
-                            onClick={() => navigate('/login')}
-                        >
-                            로그인으로 이동
-                        </button>
-                    </div>
-                </Modal>
+                <LoginRequiredModal onClose={() => setErrorMessage('')} />
             )}
         </div>
     );
 }
 
-const { colors, typography } = theme;
-
-const btnStyles = css`
-    all: unset;
-    width: 10rem;
-    height: 2.5rem;
-    border-radius: 1rem;
-    background-color: ${colors.status.normal.excellent};
-    color: ${colors.grey[100]};
-    font-weight: ${typography.fontWeight.bold};
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    cursor: pointer;
-`;
+const { colors } = theme;
 
 const overlayStyles = css`
     position: fixed;
