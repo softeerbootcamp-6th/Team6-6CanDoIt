f1dbfbedf26201318cf30e7e92f291bef994dbdc
Park Jong Seong <pjsc0903@gmail.com>
2025-08-27 15:49:17 +0900

    :sparkles: / #281 - 빈 카드일 경우 구현

--- frontend/src/components/organisms/Report/ReportCardWrapper.tsx
+++ frontend/src/components/organisms/Report/ReportCardWrapper.tsx
@@ -4,11 +4,16 @@ import { css } from '@emotion/react';
 interface PropsState {
     onClick: () => void;
     children: React.ReactNode;
+    isEmptyCard?: boolean;
 }
 
-export default function ReportCardWrapper({ onClick, children }: PropsState) {
+export default function ReportCardWrapper({
+    onClick,
+    children,
+    isEmptyCard = false,
+}: PropsState) {
     return (
-        <div onClick={onClick} css={cardStyle}>
+        <div onClick={onClick} css={cardStyle(isEmptyCard)}>
             {children}
         </div>
     );
@@ -16,7 +21,7 @@ export default function ReportCardWrapper({ onClick, children }: PropsState) {
 
 const { colors } = theme;
 
-const cardStyle = css`
+const cardStyle = (isEmptyCard: boolean) => css`
     width: 25rem;
     height: 36.25rem;
     display: flex;
@@ -25,5 +30,12 @@ const cardStyle = css`
     border-radius: 2.5rem;
     padding: 0.6rem;
     box-sizing: border-box;
-    background-color: ${colors.grey[20]};
+    background-color: ${isEmptyCard ? colors.grey[0] : colors.grey[20]};
+    border: ${isEmptyCard ? `5px dashed ${colors.grey[60]}` : 'none'};
+    ${isEmptyCard &&
+    `cursor: pointer;
+     &:hover{
+        background-color: ${colors.grey[10]};
+        border: 5px dashed ${colors.grey[90]};
+     }`}
 `;
