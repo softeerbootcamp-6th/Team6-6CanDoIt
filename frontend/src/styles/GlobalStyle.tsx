eb9f5bcc0819a19a1c4ade799c8321626fafd022
Park Jong Seong <pjsc0903@gmail.com>
2025-08-27 19:11:07 +0900

    :bug: - 폰트 적용 수정

--- frontend/src/styles/GlobalStyle.tsx
+++ frontend/src/styles/GlobalStyle.tsx
@@ -5,7 +5,38 @@ export const GlobalStyle = () => {
     return (
         <Global
             styles={css`
-                ${emotionReset}
+                ${emotionReset};
+
+                @font-face {
+                    font-family: 'Pretendard';
+                    font-weight: 400;
+                    font-style: normal;
+                    font-display: swap;
+                    src: url('/fonts/Pretendard-Regular.woff2') format('woff2');
+                }
+                @font-face {
+                    font-family: 'Pretendard';
+                    font-weight: 500;
+                    font-style: normal;
+                    font-display: swap;
+                    src: url('/fonts/Pretendard-Medium.woff2') format('woff2');
+                }
+                @font-face {
+                    font-family: 'Pretendard';
+                    font-weight: 700;
+                    font-style: normal;
+                    font-display: swap;
+                    src: url('/fonts/Pretendard-Bold.woff2') format('woff2');
+                }
+
+                html,
+                body {
+                    font-synthesis: none;
+                }
+                body {
+                    font-family: 'Pretendard', system-ui, sans-serif;
+                    font-weight: 400;
+                }
             `}
         />
     );
